dbddfef70c75bdb9ce865f7b37665fcf65339bc4
parthrohilla <parth05rohilla@gmail.com>
2022-06-12 01:29:27 +0530

    Time: 4449 ms (12.15%), Space: 13.9 MB (67.49%) - LeetHub

---
+++ 567-permutation-in-string/567-permutation-in-string.py
@@ -0,0 +1,15 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        s1Map, s2Map = Counter(s1), {}
+        
+        l = 0
+        for r in range(len(s2)):
+            if s2[r] in s1Map:
+                substring = s2[r:r + sum(s1Map.values())]
+                if Counter(s1Map) == Counter(substring):
+                    return True
+            
+        return False
+        
+        
+            
