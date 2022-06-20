aab1e7ed6bf50169a7fdcdf69f1be8b22d93bf59
parthrohilla <parth05rohilla@gmail.com>
2022-06-21 04:46:59 +0530

    Time: 244 ms (10.48%), Space: 14.2 MB (10.41%) - LeetHub

---
+++ 39-combination-sum/39-combination-sum.py
@@ -0,0 +1,17 @@
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        output = []
+        
+        def dfs(index, subset, total):
+            if total == target:
+                output.append(subset.copy())
+                return
+            
+            if index >= len(candidates) or total > target:
+                return 
+            
+            dfs(index, subset+[candidates[index]], total+candidates[index])
+            dfs(index+1, subset, total)
+        
+        dfs(0,[],0)
+        return output
