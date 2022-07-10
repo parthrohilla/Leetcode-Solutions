482150a6b96996498329fb19e5461b00fb48701b
parthrohilla <parth05rohilla@gmail.com>
2022-07-11 03:35:53 +0530

    Time: 33 ms (90.33%), Space: 13.8 MB (78.52%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.py
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.py
@@ -9,23 +9,20 @@ class Solution:
                    "8": "tuv",
                    "9": "wxyz"
                    }
-        
+
         def helper(number):
             if len(number) == 0:
                 return []
             if len(number) == 1:
-                temp = []
-                for char in num_pad[number[0]]:
-                    temp.append(char)
-                return temp
-                         
+                return list(num_pad[number[0]])
+
             small = helper(number[1:])
             output = []
             for char in num_pad[number[0]]:
                 for combination in small:
                     output.append(char + combination)
-            
+
             return output
-        
+
         return helper(digits)
         
