18ad1c3c8d564225bdda289aa301e2d13dd89924
parthrohilla <parth05rohilla@gmail.com>
2022-07-10 19:54:37 +0530

    Time: 6444 ms (67.84%), Space: 13.9 MB (50.88%) - LeetHub

--- 79-word-search/79-word-search.py
+++ 79-word-search/79-word-search.py
@@ -1,22 +1,26 @@
 class Solution:
-    def dfs(self, board: List[List[str]], word: str, i: int, j: int, visited: List[List[bool]], k: int) -> bool:
-        if k == len(word):
-            return True
-        if i < 0 or i >= len(board) or j < 0 or j >= len(board[0]) or board[i][j] != word[k] or visited[i][j]: 
-            return False
-        visited[i][j] = True
-        res1 = self.dfs(board, word, i + 1, j, visited, k + 1)
-        res2 = self.dfs(board, word, i - 1, j, visited, k + 1)
-        res3 = self.dfs(board, word, i, j + 1, visited, k + 1)
-        res4 = self.dfs(board, word, i, j - 1, visited, k + 1)
-        visited[i][j] = False
-        return (res1 or res2 or res3 or res4)
-    
     def exist(self, board: List[List[str]], word: str) -> bool:
-        for i in range(len(board)):
-            for j in range(len(board[0])):
-                visited = [[False for _ in range(len(board[0]))] for _ in range(len(board))]
-                k = 0
-                if self.dfs(board, word, i, j, visited, k):
-                    return True
+        rows, cols = len(board), len(board[0])
+        visited = [[False for _ in range(cols)] for _ in range(rows)]
+
+        def dfs(i, j, index):
+            if index == len(word):
+                return True
+            if i >= rows or i < 0 or j >= cols or j < 0 or visited[i][j] or board[i][j] != word[index]:
+                return False
+
+            visited[i][j] = True
+            res = (dfs(i+1, j, index+1) or
+                   dfs(i-1, j, index+1) or
+                   dfs(i, j+1, index+1) or
+                   dfs(i, j-1, index+1))
+
+            visited[i][j] = False
+            return res
+
+        for i in range(rows):
+            for j in range(cols):
+                if board[i][j] == word[0]:
+                    if dfs(i, j, 0):
+                        return True
         return False
