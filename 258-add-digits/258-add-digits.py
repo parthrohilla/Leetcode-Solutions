e32f3d479a59f1d288234dd32fec73a47f1238b0
parthrohilla <parth05rohilla@gmail.com>
2022-07-12 01:59:25 +0530

    Time: 46 ms (61.73%), Space: 13.8 MB (54.44%) - LeetHub

--- 258-add-digits/258-add-digits.py
+++ 258-add-digits/258-add-digits.py
@@ -1,13 +1,8 @@
 class Solution:
     def addDigits(self, n: int) -> int:
-        while n // 10:
-            n = self.sum_of_digits(n)
-        return n
-
-    def sum_of_digits(self, n):
-        ans = 0
-        while n:
-            rem = n % 10
-            ans += rem
-            n = n // 10
-        return ans
+        if n == 0:
+            return 0
+        elif n % 9 == 0:
+            return 9
+        else:
+            return n % 9
