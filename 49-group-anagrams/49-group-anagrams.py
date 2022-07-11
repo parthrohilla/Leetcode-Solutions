3f1d8d2a125fbeac9db99f7eb377ac64bb4bba54
parthrohilla <parth05rohilla@gmail.com>
2022-07-12 01:15:25 +0530

    Time: 192 ms (27.34%), Space: 20 MB (9.88%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.py
+++ 49-group-anagrams/49-group-anagrams.py
@@ -1,10 +1,14 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        string_map = defaultdict(list)
+        string_map = {}
         for string in strs:
             count = [0 for _ in range(26)]
             for char in string:
                 count[ord(char)-ord("a")] += 1
-            string_map[tuple(count)].append(string)
-        
+
+            key = tuple(count)
+            if key not in string_map:
+                string_map[key] = []
+            string_map[key].append(string)
+
         return string_map.values()
