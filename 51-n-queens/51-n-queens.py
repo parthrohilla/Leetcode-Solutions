a19a68fcbdf1d1a800b4b6714ef5458a9eb57fc5
parthrohilla <parth05rohilla@gmail.com>
2022-07-11 22:06:55 +0530

    Time: 122 ms (35.28%), Space: 14.5 MB (44.01%) - LeetHub

---
+++ 51-n-queens/51-n-queens.py
@@ -0,0 +1,42 @@
+class Solution:
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        board = [["." for _ in range(n)] for _ in range(n)]
+        col_set, neg_diagonal, pos_diagonal = set(), set(), set()
+        output = []
+
+        def backTrack(row):
+            # Base-Case
+            if row == n:
+                temp = []
+                for i in range(n):
+                    column = ""
+                    for j in range(n):
+                        column += board[i][j]
+                    temp.append(column)
+                output.append(temp)
+                return
+
+            for col in range(n):
+                if self.isNotSafe(col_set, pos_diagonal, neg_diagonal, row, col):
+                    continue
+                else:
+                    col_set.add(col)
+                    pos_diagonal.add(row+col)
+                    neg_diagonal.add(row-col)
+                    board[row][col] = "Q"
+
+                    backTrack(row+1)
+
+                    board[row][col] = "."
+                    col_set.remove(col)
+                    pos_diagonal.remove(row + col)
+                    neg_diagonal.remove(row - col)
+
+        backTrack(0)
+        return output
+
+    def isNotSafe(self, col_set, pos_diagonal, neg_diagonal, row, col) -> bool:
+        if col in col_set or (row + col) in pos_diagonal or (row - col) in neg_diagonal:
+            return True
+        else:
+            return False
