a0ac26e80eeeb7d2d4a9e258a017f202f60be7cc
parthrohilla <parth05rohilla@gmail.com>
2022-07-13 02:56:26 +0530

    Time: 797 ms (41.88%), Space: 15.2 MB (82.21%) - LeetHub

--- 55-jump-game/55-jump-game.py
+++ 55-jump-game/55-jump-game.py
@@ -1,17 +1,7 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        failed_index = [False] * len(nums)
-        @lru_cache(None)
-        def helper(index):
-            if index >= len(nums) or failed_index[index]:
-                return False
-            if index == len(nums)-1:
-                return True 
-            for i in range(nums[index], 0, -1):
-                if helper(index + i):
-                    return True   
-            failed_index[index] = True
-            return False
-        
-        return helper(0)
-            
+        goal = len(nums) - 1
+        for i in range(len(nums) - 2, -1, -1):
+            if i + nums[i] >= goal:
+                goal = i
+        return goal == 0
