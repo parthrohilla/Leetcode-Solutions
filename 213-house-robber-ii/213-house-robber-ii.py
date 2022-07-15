8a346f0600b9d69b65a4c7d85c65aa09a891ad3b
parthrohilla <parth05rohilla@gmail.com>
2022-07-15 05:43:40 +0530

    Time: 33 ms (92.38%), Space: 13.9 MB (71.22%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.py
+++ 213-house-robber-ii/213-house-robber-ii.py
@@ -1,23 +1,19 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        # 1 house = 1 solution
         if len(nums) == 1:
             return nums[0]
-        # rob all houses besides first to work around constraint
-        skip_first = self.rob_helper(nums[1:], 0, {})
-        # rob all houses besides last to work around constraint
-        skip_last = self.rob_helper(nums[:-1], 0, {})
+        
+        skip_first = self.helper(0, nums[1:], {})
+        skip_last = self.helper(0, nums[:-1], {})
+        
         return max(skip_first, skip_last)
-        
-    def rob_helper(self, houses, index, memo):
-        if index >= len(houses):
+    
+    def helper(self, i, nums, memo):
+        if i >= len(nums):
             return 0
+        if i in memo:
+            return memo[i]
         
-        if index not in memo:
-            # option 1: rob current house and skip next one
-            rob_current = houses[index] + self.rob_helper(houses, index + 2, memo)
-            # option 2: skip current house and rob next one
-            skip_current = self.rob_helper(houses, index + 1, memo)
-            memo[index] = max(rob_current, skip_current)
+        memo[i] = max(nums[i] + self.helper(i+2, nums, memo), self.helper(i+1, nums, memo))
+        return memo[i]
         
-        return memo[index]
