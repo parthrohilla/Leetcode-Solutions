1ef072df324cbc246051bb907a877a5c7b1d4256
parthrohilla <parth05rohilla@gmail.com>
2022-07-15 05:54:04 +0530

    Time: 64 ms (16.10%), Space: 13.9 MB (24.40%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.py
+++ 213-house-robber-ii/213-house-robber-ii.py
@@ -2,16 +2,18 @@ class Solution:
     def rob(self, nums: List[int]) -> int:
         if len(nums) == 1:
             return nums[0]
-
-        temp1 = self.helper(0, nums[1:], {})
-        temp2 = self.helper(0, nums[:-1], {})
-        return max(temp1, temp2)
-
-    def helper(self, i, nums, memo):
-        if i >= len(nums):
-            return 0
-        if i in memo:
+        
+        def helper(i, nums, memo):
+            if i>=len(nums):
+                return 0
+            if i in memo:
+                return memo[i]
+            
+            memo[i] = max(nums[i] + helper(i+2, nums, memo) , helper(i+1, nums, memo))
             return memo[i]
-
-        memo[i] = max(nums[i] + self.helper(i+2, nums, memo), self.helper(i+1, nums, memo))
-        return memo[i]
+            
+        nums1, nums2 = nums[1:], nums[:-1]
+        temp1 = helper(0, nums1, {})
+        temp2 = helper(0, nums2, {})
+        return max(temp1, temp2)
+        
