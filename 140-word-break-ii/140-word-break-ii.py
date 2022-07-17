3108f82e1c97b7f5bc9f92d7056f277143fd0bda
parthrohilla <parth05rohilla@gmail.com>
2022-07-18 03:21:18 +0530

    Time: 52 ms (33.84%), Space: 13.9 MB (81.48%) - LeetHub

--- 140-word-break-ii/140-word-break-ii.py
+++ 140-word-break-ii/140-word-break-ii.py
@@ -1,7 +1,6 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
         output, wordDict = [], set(wordDict)
-        memo = {}
         def dfs(string, current):
             if not string:
                 output.append(current[:-1])
@@ -10,8 +9,7 @@ class Solution:
             for i in range(len(string)):
                 left = string[:i+1]
                 if left in wordDict:
-                    dfs(string[i+1:], current + left + " ")
-            
+                    dfs(string[i+1:], current + left + " ") 
             return    
         
         dfs(s, "")
