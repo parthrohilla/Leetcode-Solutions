628e4d6755e474c73bf852ddbab8b7bdad548d29
parthrohilla <parth05rohilla@gmail.com>
2022-07-19 19:31:51 +0530

    Time: 208 ms (64.88%), Space: 17.2 MB (73.58%) - LeetHub

--- 72-edit-distance/72-edit-distance.py
+++ 72-edit-distance/72-edit-distance.py
@@ -1,20 +1,20 @@
 class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
-        memo = {}
+        memo = [[-1 for _ in range(len(word2)+1)] for _ in range(len(word1)+1)]
         def dfs(i, j):
-            if i == len(word1):
+            if i >= len(word1):
                 return len(word2)-j
-            if j == len(word2):
+            if j >= len(word2):
                 return len(word1)-i
-            if (i, j) in memo:
-                return memo[(i, j)]
+            if memo[i][j] != -1:
+                return memo[i][j]
             
             if word1[i] == word2[j]:
-                memo[(i, j)] = dfs(i+1, j+1)
-                return memo[(i, j)]
+                memo[i][j] = dfs(i+1, j+1)
+                return memo[i][j]
             
-            memo[(i, j)] = 1 + min(dfs(i, j+1), dfs(i+1, j), dfs(i+1, j+1))
-            return memo[(i, j)]
+            memo[i][j] = 1 + min(dfs(i, j+1), dfs(i+1, j), dfs(i+1, j+1))
+            return memo[i][j]
         
         
         return dfs(0, 0)
