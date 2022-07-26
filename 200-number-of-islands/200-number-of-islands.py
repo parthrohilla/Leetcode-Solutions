6a65df7256f836e878407218a9b7c459f0ad6c6a
parthrohilla <parth05rohilla@gmail.com>
2022-07-26 15:12:30 +0530

    Time: 535 ms (36.27%), Space: 16.6 MB (41.55%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -1,27 +1,23 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         m, n = len(grid), len(grid[0])
-        visited = set() 
-        
-        def bfs(i, j):
-            q = deque()
-            visited.add((i,j))
-            q.append((i, j))
-            while q:
-                r,c = q.popleft()
-                directions = [[0, 1], [1, 0], [-1, 0], [0, -1]]
-                for rx, ry in directions:
-                    row, col = r + rx, c + ry
-                    if row>=0 and row<m and col>=0 and col<n and grid[row][col] == "1" and (row,col) not in visited:
-                        q.append((row,col))
-                        visited.add((row,col))
-                        
-                        
+        def dfs(i, j):
+            if i not in range(m) or j not in range(n) or grid[i][j] != "1":
+                return
+            
+            grid[i][j] = "-1"
+            dfs(i+1, j)
+            dfs(i-1, j)
+            dfs(i, j+1)
+            dfs(i, j-1)
+
         
         islands = 0
         for i in range(m):
             for j in range(n):
-                if grid[i][j] == "1" and (i,j) not in visited:
-                    bfs(i, j)
+                if grid[i][j] == "1":
+                    dfs(i, j)
                     islands += 1
+        
         return islands
+        
