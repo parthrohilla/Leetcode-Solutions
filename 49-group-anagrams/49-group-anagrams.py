bcb1df6ca7f60315707f03ba50ef1190f750ce2e
parthrohilla <parth05rohilla@gmail.com>
2022-08-13 03:10:22 +0530

    Time: 111 ms (88.78%), Space: 19.8 MB (21.42%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.py
+++ 49-group-anagrams/49-group-anagrams.py
@@ -1,12 +1,11 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        string_map = defaultdict(list)
-        for string in strs:
-            count = [0 for _ in range(26)]
-            for char in string:
-                count[ord(char)-ord("a")] += 1
-                
-            key = tuple(count)
-            string_map[key].append(string)
-
-        return string_map.values()
+        group = defaultdict(list)
+        for str in strs:
+            encode = [0]*26
+            for ch in str:
+                encode[ord(ch)-ord("a")] += 1
+            encode = tuple(encode)
+            group[encode].append(str)
+        return group.values()
+        
