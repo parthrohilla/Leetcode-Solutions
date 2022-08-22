3abca4c263defcbcb26a3faafa2dbeedaab6e338
parthrohilla <parth05rohilla@gmail.com>
2022-08-22 21:58:57 +0530

    Time: 266 ms (58.22%), Space: 14.7 MB (30.45%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.py
+++ 785-is-graph-bipartite/785-is-graph-bipartite.py
@@ -1,20 +1,22 @@
 class Solution:
     def isBipartite(self, graph: List[List[int]]) -> bool:
         q = deque()
-        visited = [-1 for _ in range(len(graph))]
+        visited = [0 for _ in range(len(graph))]
+        
+        def dfs(node, colour):
+            if visited[node] != 0:
+                return visited[node] == colour
+            
+            visited[node] = colour
+            for n in graph[node]:
+                if not dfs(n, -1 * colour):
+                    return False
+            
+            return True
+            
         
         for node in range(len(graph)):
-            if visited[node] == -1:
-                q.append((node, 0))
-                visited[node] = 0
-                
-                while q:
-                    node, colour = q.popleft()
-                    for n in graph[node]:
-                        if visited[n] == -1:
-                            q.append((n,colour ^ 1))
-                            visited[n] = colour ^ 1
-                        elif visited[n] == colour:
-                            return False
+            if visited[node] == 0 and not dfs(node, 1):
+                return False
         
         return True
