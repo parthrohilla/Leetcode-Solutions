da88f59eef6d3d1e3950fadb6f8724957d7f895d
parthrohilla <parth05rohilla@gmail.com>
2022-08-24 20:38:39 +0530

    Time: 4306 ms (91.48%), Space: 14 MB (12.63%) - LeetHub

--- 79-word-search/79-word-search.py
+++ 79-word-search/79-word-search.py
@@ -1,26 +1,23 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        rows, cols = len(board), len(board[0])
-        visited = [[False for _ in range(cols)] for _ in range(rows)]
-
-        def dfs(i, j, index):
-            if index == len(word):
+        m,n = len(board), len(board[0])
+        visited = [[False]*n for _ in range(m)]
+        
+        def dfs(i, j, k):
+            if k == len(word):
                 return True
-            if i >= rows or i < 0 or j >= cols or j < 0 or visited[i][j] or board[i][j] != word[index]:
+            if i<0 or i>=m or j<0 or j>=n or board[i][j] != word[k] or visited[i][j]:
                 return False
-
+            
             visited[i][j] = True
-            res = (dfs(i+1, j, index+1) or
-                   dfs(i-1, j, index+1) or
-                   dfs(i, j+1, index+1) or
-                   dfs(i, j-1, index+1))
-
+            if dfs(i, j+1, k+1) or dfs(i, j-1, k+1) or dfs(i+1, j, k+1) or dfs(i-1, j, k+1):
+                return True
             visited[i][j] = False
-            return res
-
-        for i in range(rows):
-            for j in range(cols):
+            return False
+              
+        for i in range(m):
+            for j in range(n):
                 if board[i][j] == word[0]:
                     if dfs(i, j, 0):
-                        return True
+                        return True    
         return False
