191cf77c070d8d3a2ad08c2b0da9f39036095b9c
parthrohilla <parth05rohilla@gmail.com>
2022-08-26 17:51:12 +0530

    Time: 1401 ms (20.65%), Space: 19.2 MB (8.81%) - LeetHub

--- 997-find-the-town-judge/997-find-the-town-judge.py
+++ 997-find-the-town-judge/997-find-the-town-judge.py
@@ -1,12 +1,19 @@
 class Solution:
     def findJudge(self, n: int, trust: List[List[int]]) -> int:
-        temp = [0] * (n+1)
-        for a,b in trust:
-            temp[a] -= 1
-            temp[b] += 1
+        adj = [[] for _ in range(n+1)]
+        for a, b in trust:
+            adj[a].append(b)
         
-        for i in range(1,n+1):
-            if temp[i] == n-1:
-                return i
-            
-        return -1
+        temp = -1
+        for i in range(1, n+1):
+            if len(adj[i]) == 0:
+                temp = i
+                break
+                
+        for i in range(1, n+1):
+            if i != temp and temp not in adj[i]:
+                return -1
+            else:
+                continue
+        
+        return temp
