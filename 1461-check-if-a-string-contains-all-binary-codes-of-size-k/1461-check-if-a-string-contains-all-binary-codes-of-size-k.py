c4e0d52616f3e7a1213c2bf6a4d9972be797295c
parthrohilla <parth05rohilla@gmail.com>
2022-08-31 18:32:09 +0530

    Time: 1274 ms (16.38%), Space: 51.4 MB (16.52%) - LeetHub

--- 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.py
+++ 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.py
@@ -4,19 +4,7 @@ class Solution:
         for i in range(len(s)-k+1):
             seen.add(s[i:i+k])
             if len(seen) == 2**k:
-                break
+                return True
+        return False
         
-        temp = set()
-        for string in seen:
-            x = string[::-1]
-            i, t=0, 0
-            while i<k:
-                t += (int(x[i]) * (2**i))
-                i += 1
-            temp.add(t)
-        
-        for i in range(2**k):
-            if i not in temp:
-                return False
-        return True
                 
