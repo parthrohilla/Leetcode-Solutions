5a23c533313f51cc5a8b095a5ac36ae7847e12e4
parthrohilla <parth05rohilla@gmail.com>
2022-08-31 16:37:38 +0530

    Time: 4418 ms (73.24%), Space: 86.8 MB (41.24%) - LeetHub

--- 1905-count-sub-islands/1905-count-sub-islands.py
+++ 1905-count-sub-islands/1905-count-sub-islands.py
@@ -1,13 +1,12 @@
 class Solution:
     def countSubIslands(self, grid1: List[List[int]], grid2: List[List[int]]) -> int:
         m,n = len(grid1), len(grid1[0])
-
-        #DFS
+        
         directions = [[0,1], [1,0], [-1,0], [0,-1]]
         def dfs(i, j):
             if grid1[i][j] == 0:
                 return False
-                
+            
             grid2[i][j] = -1
             res = True
             for dx, dy in directions:
@@ -15,7 +14,8 @@ class Solution:
                 if x<m and x>=0 and y>=0 and y<n and grid2[x][y] == 1:
                     res = dfs(x,y) and res
             return res
-            
+        
+        
         islands = 0
         for i in range(m):
             for j in range(n):
