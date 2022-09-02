ddf0ba36bb274fc4b8ff40386dfaf8d442581183
parthrohilla <parth05rohilla@gmail.com>
2022-09-02 18:39:31 +0530

    Time: 546 ms (83.62%), Space: 15.8 MB (24.95%) - LeetHub

--- 1267-count-servers-that-communicate/1267-count-servers-that-communicate.py
+++ 1267-count-servers-that-communicate/1267-count-servers-that-communicate.py
@@ -1,20 +1,20 @@
 class Solution:
     def countServers(self, grid: List[List[int]]) -> int:
         m,n = len(grid), len(grid[0])
-        row_map = defaultdict(list)
-        col_map = defaultdict(list)
+        row_map = defaultdict(int)
+        col_map = defaultdict(int)
         
         for i in range(m):
             for j in range(n):
                 if grid[i][j] == 1:
-                    row_map[i].append((i,j))
-                    col_map[j].append((i,j))
+                    row_map[i] += 1
+                    col_map[j] += 1
         
         connected = set()
         for i in range(m):
             for j in range(n):
                 if grid[i][j] == 1:
-                    if len(row_map[i]) > 1 or len(col_map[j]) > 1:
+                    if row_map[i] > 1 or col_map[j] > 1:
                         connected.add((i,j))
         return len(connected)
                         
