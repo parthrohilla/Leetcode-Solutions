87a58e91fc52f2ad31dd723ed1f057e336cf9e01
parthrohilla <parth05rohilla@gmail.com>
2022-09-03 01:00:52 +0530

    Time: 26 ms (99.49%), Space: 13.8 MB (99.36%) - LeetHub

--- 872-leaf-similar-trees/872-leaf-similar-trees.py
+++ 872-leaf-similar-trees/872-leaf-similar-trees.py
@@ -6,14 +6,9 @@
 #         self.right = right
 class Solution:
     def leafSimilar(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> bool:
-        
         def leaf(root):
-            if not root:
-                return []
-            
-            if root.left == None and root.right == None:
-                return [root.val]
-            
-            return leaf(root.left) + leaf(root.right)
+            if not root: return []
+            if root.left == None and root.right == None: return [root.val]
+            else: return leaf(root.left) + leaf(root.right)
         
         return leaf(root1) == leaf(root2)
