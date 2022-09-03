5ddfd2e43a740f194510aea5d1bfe35eaf8aee1b
parthrohilla <parth05rohilla@gmail.com>
2022-09-04 02:29:15 +0530

    Time: 230 ms (55.07%), Space: 15.2 MB (72.51%) - LeetHub

--- 787-cheapest-flights-within-k-stops/787-cheapest-flights-within-k-stops.py
+++ 787-cheapest-flights-within-k-stops/787-cheapest-flights-within-k-stops.py
@@ -5,13 +5,11 @@ class Solution:
         
         for _ in range(k+1):
             tmp_prices = prices.copy()
-            
             for a,b,weight in flights:
                 if prices[a]==math.inf:
                     continue
                 if prices[a] + weight < tmp_prices[b]:
                     tmp_prices[b] = prices[a] + weight
             prices = tmp_prices
-        
         return prices[dst] if prices[dst] != math.inf else -1
                     
