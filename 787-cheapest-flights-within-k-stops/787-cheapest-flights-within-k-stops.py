bdc78024f9d578259e4fc1dccc3f2bab73853f6a
parthrohilla <parth05rohilla@gmail.com>
2022-09-04 02:27:59 +0530

    Time: 698 ms (6.26%), Space: 15.1 MB (89.24%) - LeetHub

---
+++ 787-cheapest-flights-within-k-stops/787-cheapest-flights-within-k-stops.py
@@ -0,0 +1,17 @@
+class Solution:
+    def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
+        prices = [math.inf] * n
+        prices[src] = 0
+        
+        for _ in range(k+1):
+            tmp_prices = prices.copy()
+            
+            for a,b,weight in flights:
+                if prices[a]==math.inf:
+                    continue
+                if prices[a] + weight < tmp_prices[b]:
+                    tmp_prices[b] = prices[a] + weight
+            prices = tmp_prices
+        
+        return prices[dst] if prices[dst] != math.inf else -1
+                    
