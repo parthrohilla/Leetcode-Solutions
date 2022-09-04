47686a9cc51bcdfedeab1227543d756ca62483cf
parthrohilla <parth05rohilla@gmail.com>
2022-09-04 19:29:49 +0530

    Time: 336 ms (92.13%), Space: 20.6 MB (53.02%) - LeetHub

---
+++ 1976-number-of-ways-to-arrive-at-destination/1976-number-of-ways-to-arrive-at-destination.py
@@ -0,0 +1,24 @@
+class Solution:
+    def countPaths(self, n: int, roads: List[List[int]]) -> int:
+        adj = defaultdict(list)
+        for a,b,time in roads:
+            adj[a].append((b,time))
+            adj[b].append((a,time))
+        
+        times = [math.inf]*n
+        times[0] = 0
+        ways = [math.inf]*n
+        ways[0] = 1
+        heap = [(0,0)]
+        
+        while heap:
+            time,node = heapq.heappop(heap)
+            
+            for b,t in adj[node]:
+                if time + t < times[b]:
+                    times[b] = time + t
+                    ways[b] = ways[node]
+                    heapq.heappush(heap,(time+t,b))
+                elif time + t == times[b]:
+                    ways[b] += ways[node]
+        return ways[-1]%(10**9 + 7)
