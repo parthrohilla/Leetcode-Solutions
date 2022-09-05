3f405f7f20a5b5740d669fed063a7be026da9418
parthrohilla <parth05rohilla@gmail.com>
2022-09-06 02:58:42 +0530

    Time: 4075 ms (5.13%), Space: 38.1 MB (98.58%) - LeetHub

---
+++ 2285-maximum-total-importance-of-roads/2285-maximum-total-importance-of-roads.py
@@ -0,0 +1,22 @@
+class Solution:
+    def maximumImportance(self, n: int, roads: List[List[int]]) -> int:
+        deg = [0]*n
+        for a,b in roads:
+            deg[a] += 1
+            deg[b] += 1
+        
+        imp = []
+        for i,val in enumerate(deg):
+            imp.append([val,i])
+        imp.sort(reverse = True)
+        
+        k = n
+        values = {}
+        for val,i in imp:
+            values[i] = k
+            k -= 1
+        
+        ans = 0
+        for a,b in roads:
+            ans += (values[a] + values[b])
+        return ans
