60087111a6bd3a4b003d29a1e517de94028e3ee4
parthrohilla <parth05rohilla@gmail.com>
2022-09-09 21:06:20 +0530

    Time: 99 ms (85.51%), Space: 14.2 MB (81.49%) - LeetHub

--- 1129-shortest-path-with-alternating-colors/1129-shortest-path-with-alternating-colors.py
+++ 1129-shortest-path-with-alternating-colors/1129-shortest-path-with-alternating-colors.py
@@ -1,14 +1,11 @@
 class Solution:
     def shortestAlternatingPaths(self, n: int, redEdges: List[List[int]], blueEdges: List[List[int]]) -> List[int]:
-        adj = defaultdict(list)
+        adj, ans = defaultdict(list), [-1]*n
         # Create an adjacency list with (TARGET, COLOUR_OF EDGE)
         for a,b in redEdges: adj[a].append((b,"red"))
         for a,b in blueEdges: adj[a].append((b,"blue"))
             
-        ans, distance = [-1]*n, 0
-        ans[0] = 0
-        visited = set()
-        q = deque()
+        ans[0], distance, visited, q = 0, 0, set(), deque()
         q.append((0,"any_color", 0))
         while q:
             node, color, distance = q.popleft()
@@ -19,5 +16,4 @@ class Solution:
                     if (nei,c) not in visited:
                         q.append((nei,c, distance+1))
                         visited.add((nei,c))
-        
         return ans
