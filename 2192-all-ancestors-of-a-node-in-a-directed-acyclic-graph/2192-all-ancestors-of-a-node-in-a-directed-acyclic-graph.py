b883ac744da103d1a52c634d097a5d559747c4d8
parthrohilla <parth05rohilla@gmail.com>
2022-09-09 19:05:30 +0530

    Time: 1677 ms (31.68%), Space: 34.1 MB (57.83%) - LeetHub

--- 2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph/2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph.py
+++ 2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph/2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph.py
@@ -1,15 +1,12 @@
 class Solution:
     def getAncestors(self, n: int, edges: List[List[int]]) -> List[List[int]]:
         adj = defaultdict(list)
-        for a,b in edges:
-            adj[b].append(a)
+        for a,b in edges: adj[b].append(a)
         
         def dfs(node):
             res = []
             
-            if node in memo: 
-                return memo[node]
-            
+            if node in memo: return memo[node]
             for nei in adj[node]:
                 res += [nei] + dfs(nei)
             
@@ -18,7 +15,6 @@ class Solution:
             
         memo = {}
         ans = [[] for _ in range(n)]
-        k = adj.keys()
         for i in range(n):
             if i in adj:
                 ans[i] = dfs(i)
