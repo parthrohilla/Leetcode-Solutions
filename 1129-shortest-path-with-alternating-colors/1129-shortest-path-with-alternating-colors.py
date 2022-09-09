2bf1b3ec7e2e648d786c0f95ef2b16b52965b202
parthrohilla <parth05rohilla@gmail.com>
2022-09-09 21:10:35 +0530

    Time: 190 ms (17.30%), Space: 14.2 MB (37.03%) - LeetHub

--- 1129-shortest-path-with-alternating-colors/1129-shortest-path-with-alternating-colors.py
+++ 1129-shortest-path-with-alternating-colors/1129-shortest-path-with-alternating-colors.py
@@ -4,15 +4,18 @@ class Solution:
         # Create an adjacency list with (TARGET, COLOUR_OF EDGE)
         for a,b in redEdges: adj[a].append((b,"red"))
         for a,b in blueEdges: adj[a].append((b,"blue"))
-            
+        
+        # Initialize variables
         ans[0], distance, visited, q = 0, 0, set(), deque()
-        q.append((0,"any_color", 0))
+        q.append((0,"any_color", 0)) # of the type(node, colour of previous edge, distance or no. of edges)
         while q:
             node, color, distance = q.popleft()
-            for nei,c in adj[node]:
+            for (nei,c) in adj[node]:
                 if c != color:
                     if ans[nei] == -1: 
                         ans[nei] = distance+1
+                    # this check is needed to prevent going in cycles that forms an infinite loop so we keep track of the edges travelled
+                    # Remember an edge is (node,color)
                     if (nei,c) not in visited:
                         q.append((nei,c, distance+1))
                         visited.add((nei,c))
