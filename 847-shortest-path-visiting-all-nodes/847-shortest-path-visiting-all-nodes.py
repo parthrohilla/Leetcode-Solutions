9124b9739254a02f19fd84bee5d074bffd4984ac
parthrohilla <parth05rohilla@gmail.com>
2022-09-11 03:21:34 +0530

    Time: 264 ms (78.90%), Space: 18.9 MB (57.81%) - LeetHub

---
+++ 847-shortest-path-visiting-all-nodes/847-shortest-path-visiting-all-nodes.py
@@ -0,0 +1,25 @@
+class Solution:
+    def shortestPathLength(self, graph: List[List[int]]) -> int:
+        n, start = len(graph), 0
+        q = deque()
+        visited = set()
+        final = ((1<<n) - 1)
+        for i in range(n):
+            visited.add((i,1<<i))
+            q.append((i,1<<i))
+        
+        distance = 0
+        while q:
+            distance += 1
+            for _ in range(len(q)):
+                node,state = q.popleft()
+                for nei in graph[node]:
+                    new_state = state | (1<<nei)
+                    if new_state == final:
+                        return distance
+                    if (nei, new_state) in visited:
+                        continue
+                    q.append((nei, new_state))
+                    visited.add((nei, new_state))
+        return 0
+    
