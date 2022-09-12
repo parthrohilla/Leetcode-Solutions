7c6c150763cae0b4610a90f06083341006ba22b6
parthrohilla <parth05rohilla@gmail.com>
2022-09-12 17:03:49 +0530

    Time: 66 ms (72.38%), Space: 14 MB (65.87%) - LeetHub

---
+++ 1002-find-common-characters/1002-find-common-characters.py
@@ -0,0 +1,31 @@
+class Solution:
+    def commonChars(self, words: List[str]) -> List[str]:
+        if len(words) == 1: return list(words[0])
+        setA, setB = [0]*26, [0]*26
+        
+        wordA, wordB = words[0], words[1]
+        for char in wordA: setA[ord(char)-ord("a")] += 1
+        for char in wordB: setB[ord(char)-ord("a")] += 1
+            
+        #intersection of sets
+        for i in range(26):
+            if setA[i] != setB[i]:
+                setA[i] = setB[i] = min(setA[i],setB[i])
+        
+        for i in range(2,len(words)):
+            setB = [0]*26
+            for char in words[i]: setB[ord(char)-ord("a")] += 1
+            for i in range(26):
+                   if setA[i] != setB[i]:
+                        setA[i] = setB[i] = min(setA[i],setB[i])
+        
+        ans = []
+        for i in range(26):
+            if setA[i] != 0:
+                for _ in range(setA[i]):
+                    ans.append(chr(ord("a") + i))
+        return ans
+                
+            
+            
+            
