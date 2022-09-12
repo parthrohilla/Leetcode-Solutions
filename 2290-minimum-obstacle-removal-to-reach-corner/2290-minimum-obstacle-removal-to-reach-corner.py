6c2c3b29dd9fd8c1aa529906400935e0829a4499
parthrohilla <parth05rohilla@gmail.com>
2022-09-12 19:22:13 +0530

    Time: 9584 ms (5.11%), Space: 37.1 MB (97.73%) - LeetHub

--- 2290-minimum-obstacle-removal-to-reach-corner/2290-minimum-obstacle-removal-to-reach-corner.py
+++ 2290-minimum-obstacle-removal-to-reach-corner/2290-minimum-obstacle-removal-to-reach-corner.py
@@ -1,20 +1,16 @@
 class Solution:
     def minimumObstacles(self, grid: List[List[int]]) -> int:
-        q = deque()
-        m,n = len(grid), len(grid[0])
+        q,m,n = deque(), len(grid), len(grid[0])
         visited = [[0]*n for _ in range(m)]
-        heap = [(0,(0,0))]
-        visited[0][0] = True
+        heap, visited[0][0] = [(0,(0,0))], True
+
         while heap:
             obs,(i,j) = heapq.heappop(heap)
             for dx,dy in [(0,1),(1,0),(-1,0),(0,-1)]:
                 x,y = i + dx, j + dy
-                if x in range(m) and y in range(n) and visited[x][y] == False:
-                    if x == m-1 and y == n-1:
-                        return obs
-                    if grid[x][y] == 1:
-                        heapq.heappush(heap,[obs+1,(x,y)])
-                    else:
-                        heapq.heappush(heap,[obs,(x,y)])
+                if x>=0 and x<m and y>=0 and y<n and visited[x][y] == False:
+                    if x == m-1 and y == n-1: return obs
+                    if grid[x][y] == 1: heapq.heappush(heap,[obs+1,(x,y)])
+                    else: heapq.heappush(heap,[obs,(x,y)])
                     visited[x][y] = True
         return 0
