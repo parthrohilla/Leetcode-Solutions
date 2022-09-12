078044dbdcfc4db20e2d7335fd1f6dea5cb76a66
parthrohilla <parth05rohilla@gmail.com>
2022-09-12 19:29:23 +0530

    Time: 9030 ms (7.38%), Space: 37.6 MB (91.48%) - LeetHub

--- 2290-minimum-obstacle-removal-to-reach-corner/2290-minimum-obstacle-removal-to-reach-corner.py
+++ 2290-minimum-obstacle-removal-to-reach-corner/2290-minimum-obstacle-removal-to-reach-corner.py
@@ -3,14 +3,12 @@ class Solution:
         q,m,n = deque(), len(grid), len(grid[0])
         visited = [[0]*n for _ in range(m)]
         heap, visited[0][0] = [(0,(0,0))], True
-
         while heap:
             obs,(i,j) = heapq.heappop(heap)
             for dx,dy in [(0,1),(1,0),(-1,0),(0,-1)]:
                 x,y = i + dx, j + dy
                 if x>=0 and x<m and y>=0 and y<n and visited[x][y] == False:
-                    if x == m-1 and y == n-1: return obs
-                    if grid[x][y] == 1: heapq.heappush(heap,[obs+1,(x,y)])
-                    else: heapq.heappush(heap,[obs,(x,y)])
+                    if x == m-1 and y == n-1: return obs 
+                    heapq.heappush(heap,[obs+ (grid[x][y]&1) ,(x,y)])
                     visited[x][y] = True
         return 0
