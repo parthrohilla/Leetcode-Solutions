2a581091bd3ede6a3bdfa01fd3d3395aa8453424
parthrohilla <parth05rohilla@gmail.com>
2022-09-13 01:09:27 +0530

    Time: 72 ms (16.75%), Space: 13.9 MB (40.71%) - LeetHub

--- 2325-decode-the-message/2325-decode-the-message.py
+++ 2325-decode-the-message/2325-decode-the-message.py
@@ -1,16 +1,15 @@
 class Solution:
     def decodeMessage(self, key: str, message: str) -> str:
-        seen, k, mapping, output = set(), "", {}, ""
+        seen, k, mapping, output = set(), "", {" ": " "}, ""
         for char in key:
             if char not in seen and char != " ":
                 k += char
                 seen.add(char)
-         
+                
         for i in range(26):
             if k[i] not in mapping:
                 mapping[k[i]] = chr(ord("a") + i)
         
-        mapping[" "] = " "
         for char in message: 
             output += mapping[char]
         
