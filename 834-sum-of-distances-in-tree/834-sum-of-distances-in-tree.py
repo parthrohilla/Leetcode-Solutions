8206cf646dd5f796e0ac3baf9316461789c75818
parthrohilla <parth05rohilla@gmail.com>
2022-09-12 15:21:31 +0530

    Time: 1805 ms (30.70%), Space: 62.2 MB (77.48%) - LeetHub

---
+++ 834-sum-of-distances-in-tree/834-sum-of-distances-in-tree.py
@@ -0,0 +1,33 @@
+class Solution:
+    def sumOfDistancesInTree(self, n: int, edges: List[List[int]]) -> List[int]:
+        adj = defaultdict(list)
+        for a,b in edges:
+            adj[a].append(b)
+            adj[b].append(a)
+        
+        count, res = [0]*n, [0]*n
+        def postorder(i, parent = -1):
+            for nei in adj[i]:
+                if nei == parent:
+                    continue
+                postorder(nei,i)
+                count[i] += count[nei]
+                res[i] += (res[nei] + count[nei])
+            count[i] += 1
+        
+        def preorder(i, parent = -1):
+            for nei in adj[i]:
+                if nei == parent:
+                    continue
+                res[nei] = res[i] - count[nei] + (len(count)-count[nei])
+                preorder(nei,i)
+        
+        postorder(0)
+        preorder(0)
+        return res
+                
+        
+        
+        
+        
+        
