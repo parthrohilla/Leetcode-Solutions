972558acab796d2c79876bb2860e911f5211fad9
Parth Rohilla <parth05rohilla@gmail.com>
2022-09-13 15:04:38 +0530

    Time: 244 ms (27.52%), Space: 14.4 MB (10.40%) - LeetHub

--- 1034-coloring-a-border/1034-coloring-a-border.py
+++ 1034-coloring-a-border/1034-coloring-a-border.py
@@ -1,19 +1,16 @@
 class Solution:
     def colorBorder(self, grid: List[List[int]], row: int, col: int, color: int) -> List[List[int]]:
-        m,n = len(grid), len(grid[0])
-        visited = set()
-        
+        m,n,visited = len(grid), len(grid[0]), set()
         def dfs(i,j):
             if (i,j) in visited: return 1
-            if i<0 or i>=m or j<0 or j>=n or grid[i][j] != x: return 0
+            if i<0 or i>=m or j<0 or j>=n or grid[i][j] != color_old: return 0
             
             visited.add((i,j))
             if dfs(i+1,j) + dfs(i,j+1) + dfs(i-1,j) + dfs(i,j-1) < 4:
                 grid[i][j] = color
             return 1
-            
-            
-        x =  grid[row][col]
+                
+        color_old =  grid[row][col]
         dfs(row, col)
         return grid
     
