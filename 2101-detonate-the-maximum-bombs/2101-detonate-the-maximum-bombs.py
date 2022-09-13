16a36d781659aee29b675ee9845f75e06bf0290b
Parth Rohilla <parth05rohilla@gmail.com>
2022-09-13 19:26:57 +0530

    Time: 2083 ms (30.85%), Space: 14.5 MB (37.40%) - LeetHub

--- 2101-detonate-the-maximum-bombs/2101-detonate-the-maximum-bombs.py
+++ 2101-detonate-the-maximum-bombs/2101-detonate-the-maximum-bombs.py
@@ -1,6 +1,13 @@
 class Solution:
     def maximumDetonation(self, bombs: List[List[int]]) -> int:
-        n, adj = len(bombs), defaultdict(list)
+        n, adj, visited, ans = len(bombs), defaultdict(list), set(), 0
+        # DFS Method
+        def dfs(i):
+            visited.add(i)
+            for nei in adj[i]:
+                if nei not in visited:
+                    dfs(nei)
+        # Building adjacency list
         for i in range(n):
             for j in range(n):
                 if i != j:
@@ -8,17 +15,9 @@ class Solution:
                     bx,by,br = bombs[j]
                     if (ax-bx)**2 + (ay-by)**2 <= ar**2: 
                         adj[i].append(j)
-
-        def dfs(i):
-            visited.add(i)
-            for nei in adj[i]:
-                if nei not in visited:
-                    dfs(nei)
-        
-        ans = 0
-        visited = set()
-        for i in range(n):
+        # checking detonations from each bomb
+        for bomb in range(n):
             visited = set()
-            dfs(i)
+            dfs(bomb)
             ans = max(ans, len(visited))
         return ans
