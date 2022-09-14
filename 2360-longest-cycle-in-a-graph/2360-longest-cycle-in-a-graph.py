f69f917c57d7e4265af117bda815fe4e24fe0015
Parth Rohilla <parth05rohilla@gmail.com>
2022-09-14 20:09:40 +0530

    Time: 4049 ms (5.02%), Space: 185.8 MB (12.59%) - LeetHub

--- 2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph.py
+++ 2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph.py
@@ -1,18 +1,22 @@
 class Solution:
     def longestCycle(self, edges: List[int]) -> int:
         n,visit=len(edges),set()
-        ranks=[float('inf')]*n
+        ranks=[math.inf]*n
 
-        def t(i,rank):
+        # i is the node and k is the position in the current cycle, position would be needed to calculate the cycle-length
+        def dfs(i,k):
+            # if a node is already visited as part of a previous cycle we return -1
+            # if edges[i] == -1 means that if it is the last node in a traversal, no need to call dfs() forther since there won't be any cycles found
             if i in visit or edges[i]==-1: 
                 return -1
-
-            if ranks[i]<rank: 
-                return rank-ranks[i]
-
-            ranks[i]=rank
-            val=t(edges[i],rank+1)
+            # Since ranks maintains the first position the node was seen, if we visite it again it means that we have encountered a cycle
+            if ranks[i]<k: 
+                return k-ranks[i]
+            # if cycle not found then update rank and call dfs() from the neighbour and mark the current node as visited
+            ranks[i] = k
+            val = dfs(edges[i],k+1)
             visit.add(i)
             return val
 
-        return max(t(i,0) for i in range(n))
+        # Call dfs() on every node starting from 0
+        return max(dfs(i,0) for i in range(n))
