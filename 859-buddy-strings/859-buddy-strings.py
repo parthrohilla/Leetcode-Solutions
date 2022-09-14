073bff7d0be60a449e9233fc3a3fbbff1629e963
Parth Rohilla <parth05rohilla@gmail.com>
2022-09-15 02:03:40 +0530

    Time: 58 ms (43.18%), Space: 14.3 MB (9.52%) - LeetHub

---
+++ 859-buddy-strings/859-buddy-strings.py
@@ -0,0 +1,27 @@
+class Solution:
+    def buddyStrings(self, s: str, goal: str) -> bool:
+        if len(s) != len(goal): return False
+        if s == goal:
+            if len(s) == len(set(s)):return False
+            return True
+        
+        s = list(s)
+        goal = list(goal)
+        
+        i = 0
+        first,second = -1,-1
+        while i < len(s):
+            if s[i] == goal[i]: 
+                i += 1
+            else:
+                if first == -1:
+                    first = i
+                else:
+                    second = i
+                    break
+                i += 1
+        
+        s[first],s[second] = s[second],s[first]
+        if s == goal:return True
+        return False
+            
