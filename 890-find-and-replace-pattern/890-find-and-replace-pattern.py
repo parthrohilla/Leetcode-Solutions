b02c1dffb5c94c883899affcb8a8807a509b39f7
Parth Rohilla <parth05rohilla@gmail.com>
2022-09-15 19:49:58 +0530

    Time: 65 ms (15.49%), Space: 14 MB (30.38%) - LeetHub

--- 890-find-and-replace-pattern/890-find-and-replace-pattern.py
+++ 890-find-and-replace-pattern/890-find-and-replace-pattern.py
@@ -1,11 +1,12 @@
 class Solution:
     def findAndReplacePattern(self, words: List[str], pattern: str) -> List[str]:
-        def p(word):
+        def encode(word):
             m = {}
             encoding = []
             for char in word:
                 encoding.append(m.setdefault(char,len(m)))
             return encoding
         
-        return [word for word in words if p(word) == p(pattern)]
+        x = encode(pattern)
+        return [word for word in words if encode(word) == x]
                     
