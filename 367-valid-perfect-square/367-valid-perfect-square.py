f951d0b137061d6ec697843599dca69c2c31a708
Parth Rohilla <parth05rohilla@gmail.com>
2022-09-17 03:31:13 +0530

    Time: 60 ms (28.45%), Space: 13.9 MB (55.29%) - LeetHub

--- 367-valid-perfect-square/367-valid-perfect-square.py
+++ 367-valid-perfect-square/367-valid-perfect-square.py
@@ -1,14 +1,12 @@
 class Solution:
     def isPerfectSquare(self, num: int) -> bool:
         if num == 1: return True
+        
         low, high = 1,num//2
         while low<=high:
             m = (low+high)//2
-            if m*m == num:
-                return True
-            elif m*m < num:
-                low = m+1
-            else:
-                high = m-1
+            if m*m == num: return True
+            elif m*m < num: low = m+1
+            else: high = m-1
         return False
             
