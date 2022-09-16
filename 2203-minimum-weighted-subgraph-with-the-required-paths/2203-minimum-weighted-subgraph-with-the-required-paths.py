33728d90085279df129c45041072bd97ac44e33f
Parth Rohilla <parth05rohilla@gmail.com>
2022-09-17 04:25:56 +0530

    Time: 5818 ms (8.74%), Space: 97 MB (42.72%) - LeetHub

--- 2203-minimum-weighted-subgraph-with-the-required-paths/2203-minimum-weighted-subgraph-with-the-required-paths.py
+++ 2203-minimum-weighted-subgraph-with-the-required-paths/2203-minimum-weighted-subgraph-with-the-required-paths.py
@@ -1,17 +1,15 @@
 class Solution:
     def minimumWeight(self, n: int, edges: List[List[int]], src1: int, src2: int, dest: int) -> int:
-        e = defaultdict(list)
-        er = defaultdict(list)
+        e, er = defaultdict(list), defaultdict(list)
         for a,b,w in edges:
             e[a].append([b,w])
             er[b].append([a,w])
         
         
-        def shortest(adj,src):
-            distance = [math.inf]*n
+        def djiktras(adj,src):
+            distance, heap, visited = [math.inf]*n, [[0,src]], set()
             distance[src] = 0
-            heap = [[0,src]]
-            visited = set()
+            
             while heap:
                 d,node = heapq.heappop(heap)   
                 if node in visited: continue
@@ -23,12 +21,13 @@ class Solution:
                             heapq.heappush(heap,[distance[nei],nei])
             return distance
         
-        d1 = shortest(e,src1)
-        d2 = shortest(e,src2)
-        dd = shortest(er,dest)
         
-        d = math.inf
+        d1 = djiktras(e,src1)
+        d2 = djiktras(e,src2)
+        dd = djiktras(er,dest)
+        
+        ans = math.inf
         for i in range(n):
-            d = min(d,d1[i] + d2[i] + dd[i])
-        return -1 if d >= math.inf else d
+            ans = min(ans,d1[i] + d2[i] + dd[i])
+        return -1 if ans >= math.inf else ans
         
