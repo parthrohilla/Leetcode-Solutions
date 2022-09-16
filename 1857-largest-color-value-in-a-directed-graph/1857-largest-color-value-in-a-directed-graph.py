dedb2144d0550e8cbf6af277dd726f9360d62d81
Parth Rohilla <parth05rohilla@gmail.com>
2022-09-16 18:52:44 +0530

    Time: 3799 ms (58.17%), Space: 169 MB (18.75%) - LeetHub

--- 1857-largest-color-value-in-a-directed-graph/1857-largest-color-value-in-a-directed-graph.py
+++ 1857-largest-color-value-in-a-directed-graph/1857-largest-color-value-in-a-directed-graph.py
@@ -2,7 +2,7 @@ class Solution:
     def largestPathValue(self, colors: str, edges: List[List[int]]) -> int:
         n, adj, visited, dfs_visited = len(colors), defaultdict(list),set(), set()
         for a,b in edges: adj[a].append(b)
-        
+        # Return -1 if it's a cycliv graph
         def dfs_cycle(i):
             visited.add(i)
             dfs_visited.add(i)
@@ -15,16 +15,21 @@ class Solution:
         for i in range(n):
             if i not in visited and dfs_cycle(i): return -1
         
-        @lru_cache(None)
+        # Calculate color path from each node
+        memo = {}
         def dfs(i, color):
-            count = 0
-            if color == colors[i]:
-                count += 1
+            if (i,color) in memo: 
+                return memo[(i,color)]
             
+            count = 0
+            if color == colors[i]: 
+                count += 1
             mx = 0
             for nei in adj[i]:
                 mx = max(mx,dfs(nei,color))
-            return mx + count
+            
+            memo[(i,color)] = mx + count
+            return memo[(i,color)]
 
         res = 0
         for i in range(n):
