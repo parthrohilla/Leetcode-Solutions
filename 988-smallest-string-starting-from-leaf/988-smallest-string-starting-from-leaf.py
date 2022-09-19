38caa3ee88ccf946390e02500f24ffcb8f8d545c
Parth Rohilla <parth05rohilla@gmail.com>
2022-09-19 16:15:57 +0530

    Time: 82 ms (46.75%), Space: 15.4 MB (97.89%) - LeetHub

--- 988-smallest-string-starting-from-leaf/988-smallest-string-starting-from-leaf.py
+++ 988-smallest-string-starting-from-leaf/988-smallest-string-starting-from-leaf.py
@@ -6,20 +6,16 @@
 #         self.right = right
 class Solution:
     def smallestFromLeaf(self, root: TreeNode) -> str:
+        self.res = None
         def dfs(root, s):
-            s = s + chr(ord('a') + root.val)
-            if not root.left and not root.right:
-                if res[0] == None:
-                    res[0] = s[::-1]
-                else:
-                    res[0] = min(res[0], s[::-1])
-            if root.left:
-                dfs(root.left, s)
-            if root.right:
-                dfs(root.right, s)
+            if not root: return ""
         
-        if not root:
-            return ''
-        res = [None]
+            s += chr(ord('a') + root.val)
+            if not root.left and not root.right:
+                if not self.res: self.res = s[::-1]
+                else: self.res = min(self.res, s[::-1])
+            if root.left: dfs(root.left, s)
+            if root.right: dfs(root.right, s)
+                
         dfs(root, '')
-        return res[0]
+        return self.res
