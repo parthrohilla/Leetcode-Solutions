39900e92748f152b855f04eac3babc4da1188e60
Parth Rohilla <parth05rohilla@gmail.com>
2022-09-19 13:52:03 +0530

    Time: 842 ms (17.33%), Space: 17.9 MB (87.30%) - LeetHub

--- 687-longest-univalue-path/687-longest-univalue-path.py
+++ 687-longest-univalue-path/687-longest-univalue-path.py
@@ -8,13 +8,12 @@ class Solution:
     def longestUnivaluePath(self, root: Optional[TreeNode]) -> int:
         if not root: return 0
         self.ans = 0
+        # dfs() returns the longest path starting from the current node 
         def dfs(node):
             temp1, temp2 = 0,0
-            if node.left: 
-                temp1 = dfs(node.left)
-            if node.right: 
-                temp2 = dfs(node.right)
-                
+            if node.left: temp1 = dfs(node.left)
+            if node.right: temp2 = dfs(node.right)
+            # Taking care of all the conditions if we need to combine results from left sub-tree or right sub-tree or from neither
             if node.left and node.right and node.val == node.left.val and node.val == node.right.val:
                 self.ans = max(self.ans, 2 + temp1 + temp2)
                 return 1 + max(temp1, temp2)
@@ -24,8 +23,8 @@ class Solution:
             elif node.right and node.right.val == node.val:
                 self.ans = max(self.ans, 1 + temp2)
                 return 1 + temp2
-            
-            return 0
+            else:
+                return 0
         
         dfs(root)
         return self.ans
