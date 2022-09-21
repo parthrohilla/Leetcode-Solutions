4305b6837235abd79f84b787aa591bf334c6cfe3
Parth Rohilla <parth05rohilla@gmail.com>
2022-09-21 16:22:33 +0530

    Time: 184 ms (5.10%), Space: 17.3 MB (8.51%) - LeetHub

--- 120-triangle/120-triangle.py
+++ 120-triangle/120-triangle.py
@@ -4,8 +4,6 @@ class Solution:
         def dfs(r, prev):
             if r == n: return 0
             if (r,prev) in memo: return memo[(r,prev)]
-            op1 = triangle[r][prev] + dfs(r+1,prev)
-            op2 = triangle[r][prev+1] + dfs(r+1, prev+1)
-            memo[(r,prev)] = min(op1, op2)
+            memo[(r,prev)] = min(triangle[r][prev] + dfs(r+1,prev), triangle[r][prev+1] + dfs(r+1, prev+1))
             return memo[(r,prev)]
         return triangle[0][0] + dfs(1, 0)
