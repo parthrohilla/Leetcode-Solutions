35a18aa48c44ece9cffb888b5183533143bc8662
Parth Rohilla <parth05rohilla@gmail.com>
2022-09-26 03:17:53 +0530

    Time: 119 ms (100.00%), Space: 14.3 MB (91.67%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.py
+++ 2418-sort-the-people/2418-sort-the-people.py
@@ -1,4 +1,3 @@
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
-        temp = sorted([[num,i] for i,num in enumerate(heights)])[::-1]
-        return [names[i] for _,i in temp]
+        return [names[i] for _,i in sorted([[num,i] for i,num in enumerate(heights)])[::-1]]
