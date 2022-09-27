7cea8671023794c9fb2b3c32d194baed275899cd
Parth Rohilla <parth05rohilla@gmail.com>
2022-09-27 21:12:13 +0530

    Time: 7726 ms (5.00%), Space: 489.2 MB (5.44%) - LeetHub

--- 322-coin-change/322-coin-change.py
+++ 322-coin-change/322-coin-change.py
@@ -2,12 +2,9 @@ class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
         memo = {}
         def dfs(i, amount) -> int:
-            if amount == 0:
-                return 0
-            if i>=len(coins) or amount < 0:
-                return math.inf
-            if (i, amount) in memo:
-                return memo[(i, amount)]
+            if amount == 0: return 0
+            if i>=len(coins) or amount < 0: return math.inf
+            if (i, amount) in memo: return memo[(i, amount)]
             
             memo[(i, amount)] = min(1 + dfs(i, amount-coins[i]), dfs(i+1, amount))
             return memo[(i, amount)]
