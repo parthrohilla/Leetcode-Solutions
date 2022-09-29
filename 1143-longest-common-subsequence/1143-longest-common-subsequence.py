b3a5f077d45255e372750ad46fbf7e052b00266e
Parth Rohilla <parth05rohilla@gmail.com>
2022-09-29 20:33:11 +0530

    Time: 3101 ms (5.00%), Space: 638.9 MB (5.12%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.py
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -1,17 +1,14 @@
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        memo = {}
+        lookup = {}
         def dfs(s1, s2):
-            if not s1 or not s2:
-                return 0
-            if (s1, s2) in memo:
-                return memo[(s1, s2)]
-            ans = 0
+            if not s1 or not s2: return 0
+            if (s1,s2) in lookup: return lookup[(s1,s2)]
+            
             if s1[0] == s2[0]:
-                ans = 1 + dfs(s1[1:], s2[1:])
+                lookup[(s1,s2)] = 1 + dfs(s1[1:], s2[1:])
             else:
-                ans = max(dfs(s1, s2[1:]), dfs(s1[1:], s2))
-            memo[(s1, s2)] = ans
-            return ans
+                lookup[(s1,s2)] = max(dfs(s1,s2[1:]), dfs(s1[1:], s2))
+            return lookup[(s1,s2)]
         
         return dfs(text1, text2)
