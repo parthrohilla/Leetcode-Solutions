3721e1204a5361c1b31bd39f1a578ea953c1aeee
Parth Rohilla <parth05rohilla@gmail.com>
2022-09-30 19:59:38 +0530

    Time: 1053 ms (5.75%), Space: 31 MB (10.33%) - LeetHub

--- 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.py
+++ 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.py
@@ -3,10 +3,9 @@ class Solution:
         m,n = len(word1), len(word2)
         lookup = {}
         def dfs(i,j):
-            if i == m or j == n:
-                return max(m-i,n-j)
+            if i == m or j == n: return max(m-i,n-j)
             if (i,j) in lookup: return lookup[(i,j)]
-            if word1[i] == word2[j]:
+            if word1[i] == word2[j]: 
                 lookup[(i,j)] = dfs(i+1, j+1)
             else:
                 lookup[(i,j)] = 1 + min(dfs(i+1,j), dfs(i,j+1))
