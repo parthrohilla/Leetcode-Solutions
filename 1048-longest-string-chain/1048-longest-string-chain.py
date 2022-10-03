1ca19c524c6b3efa065a839ea879140a2a68f4e0
Parth Rohilla <parth05rohilla@gmail.com>
2022-10-04 01:24:17 +0530

    Time: 6274 ms (5.02%), Space: 14.3 MB (88.18%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.py
+++ 1048-longest-string-chain/1048-longest-string-chain.py
@@ -5,11 +5,11 @@ class Solution:
         dp = [1] * n
         for i in range(n):
             for j in range(i):
-                if self.helper(words[i],words[j]):
+                if self.isChain(words[i],words[j]):
                     dp[i] = max(dp[i], 1 + dp[j])
         return max(dp)
     
-    def helper(self, s1, s2):
+    def isChain(self, s1, s2):
         if len(s1) - len(s2) != 1: return False
         for i in range(len(s1)):
             if s1[:i] + s1[i+1:] == s2: return True
