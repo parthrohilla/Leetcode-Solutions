2be38d849df735d5995edbff0c16b80b3520ba75
Parth Rohilla <parth05rohilla@gmail.com>
2022-10-04 02:10:17 +0530

    Time: 65 ms (27.08%), Space: 14 MB (48.47%) - LeetHub

--- 993-cousins-in-binary-tree/993-cousins-in-binary-tree.py
+++ 993-cousins-in-binary-tree/993-cousins-in-binary-tree.py
@@ -6,23 +6,16 @@
 #         self.right = right
 class Solution:
     def isCousins(self, root: Optional[TreeNode], x: int, y: int) -> bool:
-        
         def dfs(curr, target, height, parent):
-            if not curr:
-                return [-1,-1]
-            if curr.val == target:
-                return [height, parent]
+            if not curr: return [-1,-1]
+            if curr.val == target: return [height, parent]
             
             [lh,lp] = dfs(curr.left, target, height+1, curr)
             [rh,rp] = dfs(curr.right, target, height+1, curr)
-            if (lh,lp) != (-1,-1):
-                return [lh,lp]
-            else:
-                return [rh,rp]
+            if (lh,lp) != (-1,-1): return [lh,lp]
+            else: return [rh,rp]
         
         [lh,lp] = dfs(root, x, 0, -1)
         [rh,rp] = dfs(root, y, 0, -1)
-        if lh == rh and lp != rp:
-            return True
-        else:
-            return False
+        if lh == rh and lp != rp: return True
+        else: return False
