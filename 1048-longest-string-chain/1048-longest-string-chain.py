0120d225a2935a50cd7f8018e0c3945b3c5c0ae6
Parth Rohilla <parth05rohilla@gmail.com>
2022-10-04 01:23:23 +0530

    Time: 2242 ms (17.79%), Space: 14.3 MB (68.49%) - LeetHub

---
+++ 1048-longest-string-chain/1048-longest-string-chain.py
@@ -0,0 +1,16 @@
+class Solution:
+    def longestStrChain(self, words: List[str]) -> int:
+        n = len(words)
+        words.sort(key = lambda x: len(x))
+        dp = [1] * n
+        for i in range(n):
+            for j in range(i):
+                if self.helper(words[i],words[j]):
+                    dp[i] = max(dp[i], 1 + dp[j])
+        return max(dp)
+    
+    def helper(self, s1, s2):
+        if len(s1) - len(s2) != 1: return False
+        for i in range(len(s1)):
+            if s1[:i] + s1[i+1:] == s2: return True
+        return False
