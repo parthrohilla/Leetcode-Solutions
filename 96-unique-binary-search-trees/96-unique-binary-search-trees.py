bfcba0be47379361d70546e7d3ea0d7a69e987d8
Parth Rohilla <parth05rohilla@gmail.com>
2022-10-04 16:10:18 +0530

    Time: 31 ms (93.56%), Space: 13.8 MB (62.55%) - LeetHub

--- 96-unique-binary-search-trees/96-unique-binary-search-trees.py
+++ 96-unique-binary-search-trees/96-unique-binary-search-trees.py
@@ -1,12 +1,3 @@
 class Solution:
-    def numTrees(self, N: int) -> int:
-        memo = [-1]*(N+1)
-        def f(n):
-            if n <= 1: return 1
-            if memo[n] != -1: return memo[n]
-            count = 0
-            for i in range(1,n+1):
-                count += f(i-1)*f(n-i)
-            memo[n] = count
-            return count
-        return f(N)
+    def numTrees(self, n: int) -> int:
+        return factorial(2*n) // (factorial(n+1) * factorial(n))
