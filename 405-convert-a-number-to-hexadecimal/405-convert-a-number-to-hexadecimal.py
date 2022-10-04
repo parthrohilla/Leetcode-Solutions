0aa9be0042807689b8d3560311f515b7db4ead8f
Parth Rohilla <parth05rohilla@gmail.com>
2022-10-04 17:56:52 +0530

    Time: 63 ms (16.27%), Space: 13.8 MB (59.91%) - LeetHub

--- 405-convert-a-number-to-hexadecimal/405-convert-a-number-to-hexadecimal.py
+++ 405-convert-a-number-to-hexadecimal/405-convert-a-number-to-hexadecimal.py
@@ -3,7 +3,8 @@ class Solution:
         if num == 0: return "0"
         mapping = {0:"0",1:"1",2:"2",3:"3",4:"4",5:"5",6:"6",7:"7",8:"8",9:"9",10:"a",11:"b",12:"c",13:"d",14:"e",15:"f"}
         ans = ""
-        num &= 0xFFFFFFFF
+        if num < 0:
+            num = 2**32+num
         while num:
             rem = num % 16
             ans = mapping[rem] + ans
