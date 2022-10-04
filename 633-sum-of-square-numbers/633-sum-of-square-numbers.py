d8bc3dfab6adcfc96c4ebafa8059ff3460bff842
Parth Rohilla <parth05rohilla@gmail.com>
2022-10-04 12:58:43 +0530

    Time: 814 ms (20.21%), Space: 13.8 MB (64.14%) - LeetHub

--- 633-sum-of-square-numbers/633-sum-of-square-numbers.py
+++ 633-sum-of-square-numbers/633-sum-of-square-numbers.py
@@ -2,7 +2,7 @@ class Solution:
     def judgeSquareSum(self, c: int) -> bool:
         a = 0
         while a*a <= c:
-            temp = c - a*a
-            if temp == math.isqrt(temp) ** 2: return True
+            b_square = c - a*a
+            if b_square == math.isqrt(b_square) ** 2: return True
             a += 1
         return False
