f3dd0cee890d942cd25ddfd2645bcd3671a000d5
Parth Rohilla <parth05rohilla@gmail.com>
2022-10-06 02:12:09 +0530

    Time: 507 ms (24.67%), Space: 14.7 MB (23.38%) - LeetHub

--- 1447-simplified-fractions/1447-simplified-fractions.py
+++ 1447-simplified-fractions/1447-simplified-fractions.py
@@ -2,11 +2,11 @@ class Solution:
     def simplifiedFractions(self, n: int) -> List[str]:
         ans = []
         seen = set()
-        for a in range(1,n):
-            for d in range(2,n+1):
-                if (a/d) < 1 and (a/d) not in seen:
-                    ans.append(str(a) + "/" + str(d))
-                    seen.add(a/d)
+        for numerator in range(1,n):
+            for denominator in range(numerator+1,n+1):
+                if (numerator/denominator) < 1 and (numerator/denominator) not in seen:
+                    ans.append(str(numerator) + "/" + str(denominator))
+                    seen.add(numerator/denominator)
         return ans
             
                 
