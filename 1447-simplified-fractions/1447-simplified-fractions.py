73dd479fa2e267c7a6eb8bc72a66a5971d5000cb
Parth Rohilla <parth05rohilla@gmail.com>
2022-10-06 02:12:28 +0530

    Time: 193 ms (93.18%), Space: 14.7 MB (23.38%) - LeetHub

--- 1447-simplified-fractions/1447-simplified-fractions.py
+++ 1447-simplified-fractions/1447-simplified-fractions.py
@@ -4,7 +4,7 @@ class Solution:
         seen = set()
         for numerator in range(1,n):
             for denominator in range(numerator+1,n+1):
-                if (numerator/denominator) < 1 and (numerator/denominator) not in seen:
+                if (numerator/denominator) not in seen:
                     ans.append(str(numerator) + "/" + str(denominator))
                     seen.add(numerator/denominator)
         return ans
