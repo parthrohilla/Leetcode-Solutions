efd4fea5f24da717311658a368c5bc24134ee78e
Parth Rohilla <parth05rohilla@gmail.com>
2022-10-05 20:46:16 +0530

    Time: 6200 ms (5.00%), Space: 17.6 MB (39.36%) - LeetHub

--- 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.py
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.py
@@ -1,16 +1,16 @@
 class Solution:
     def minCost(self, n: int, cuts: List[int]) -> int:
-        @lru_cache(None)
+        memo = {}
         def dfs(i,j):
             if i>j: return 0
+            if (i,j) in memo: return memo[(i,j)]
             
             mini = math.inf
             for k in range(i,j+1,1):
                 mini = min(mini, cuts[j+1] - cuts[i-1] + dfs(i,k-1) + dfs(k+1,j))
+            memo[(i,j)] = mini
             return mini
         
-        c = len(cuts)
         cuts.sort()
         cuts = [0] + cuts + [n]
-        print(len(cuts))
-        return dfs(1,c)
+        return dfs(1,len(cuts)-2)
