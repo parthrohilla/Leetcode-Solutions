7c9f76e89e5ce8f04dc31f2431d9ff6a7252472d
Parth Rohilla <parth05rohilla@gmail.com>
2022-10-07 03:28:12 +0530

    Time: 77 ms (26.18%), Space: 15.2 MB (45.94%) - LeetHub

--- 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.py
+++ 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.py
@@ -6,19 +6,32 @@
 #         self.right = right
 class Solution:
     def flatten(self, root: Optional[TreeNode]) -> None:
+        # dfs() returns head(ROOT) and Tail of the list
         def dfs(node):
-            if not node: return [node,node]
+            if not node: 
+                return [node,node]
+            # Recursve Calls on Left Sub-tree and Right Sub-Tree
             left, tailL = dfs(node.left)
             right, tailR = dfs(node.right)
-            
+             # Combine the risult of left subTree into the Right Half to "flatten it"
+             # ROOT 
+             #    \
+             #     \         
+             #      \
+             #    [Left sub-Tree HEAD]
+             #        \             
+             #         \
+             #          \  
+             #        [Left sub-Tree TAIL]
+             #            \
+             #             \  
+             #              \
+             #        [Right sub-Tree flattened result] 
             if left: node.right = left
             if tailL: tailL.right = right
-            node.left = None
-            
-            tail = node
+            node.left, tail = None, node
             while tail.right: tail = tail.right
             return [node, tail]
-        
         return dfs(root)[0]
             
         
