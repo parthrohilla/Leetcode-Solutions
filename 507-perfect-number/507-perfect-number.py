5d7295079113a283e9c8592ae3db94a12dd38f50
Parth Rohilla <parth05rohilla@gmail.com>
2022-10-07 03:56:59 +0530

    Time: 146 ms (5.22%), Space: 13.8 MB (96.13%) - LeetHub

--- 507-perfect-number/507-perfect-number.py
+++ 507-perfect-number/507-perfect-number.py
@@ -1,12 +1,9 @@
 class Solution:
     def checkPerfectNumber(self, num: int) -> bool:
-        k = 1
-        i = 2
+        divisor_sum, i = 1, 2
         while i < pow(num,0.5):
-            if num % i == 0:
-                k += i + (num//i)
+            if num % i == 0: divisor_sum += i + (num//i)
             i += 1
-        
-        if pow(num,0.5)**2 == num: k += i
-        return k == num
+        if pow(num,0.5)**2 == num: divisor_sum += i
+        return divisor_sum == num
         
