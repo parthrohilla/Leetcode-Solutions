31eab25af1d7dc6d2ceb01c34682eeffefe95822
Parth Rohilla <parth05rohilla@gmail.com>
2022-10-07 16:11:33 +0530

    Time: 3353 ms (5.01%), Space: 28 MB (80.02%) - LeetHub

--- 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.py
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.py
@@ -1,27 +1,23 @@
 class Solution:
     def largestRectangleArea(self, heights: List[int]) -> int:
         n = len(heights)
-        left = [-1] * n
-        right = [n] * n
+        left, right = [-1]*n, [n]*n
         
-        # populate left and right
         stack = []
-        for i,h in enumerate(heights):
-            while stack and h < heights[stack[-1]]:
-                pop = stack.pop()
-                right[pop] = i
+        for i in range(n):
+            while stack and heights[i] < heights[stack[-1]]: right[stack.pop()] = i
             stack.append(i)
         
         stack = []
         for i in range(n-1,-1,-1):
-            while stack and heights[i] < heights[stack[-1]]:
-                pop = stack.pop()
-                left[pop] = i
+            while stack and heights[i] < heights[stack[-1]]: left[stack.pop()] = i
             stack.append(i)
-            
-        # calculate ans
+        
         area = 0
+        print(left, right)
         for i,h in enumerate(heights):
-            width = right[i] - left[i] - 1
-            area = max(area, width * h)
+            base = right[i]-left[i]-1
+            area = max(area, h*base)
         return area
+        
+        
