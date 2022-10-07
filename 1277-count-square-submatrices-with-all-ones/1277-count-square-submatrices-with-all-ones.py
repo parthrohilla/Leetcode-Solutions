d9414ff919865e4b5ae149774b5983d9ddd101ef
Parth Rohilla <parth05rohilla@gmail.com>
2022-10-07 17:06:11 +0530

    Time: 1193 ms (47.54%), Space: 16.3 MB (33.28%) - LeetHub

--- 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.py
+++ 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.py
@@ -5,11 +5,9 @@ class Solution:
         
         for i in range(m):
             for j in range(n):
-                if i == 0 or j == 0:
-                    if matrix[i][j] == 1:
-                        dp[i][j] = 1
+                if (i == 0 or j == 0) and matrix[i][j] == 1:
+                    dp[i][j] = 1
                 elif matrix[i][j] == 1:
                     dp[i][j] = 1 + min(dp[i-1][j], dp[i][j-1], dp[i-1][j-1])
-        
         return sum([sum(x) for x in dp])
                 
