179c1238fbeb5366863df90b548006087f3b12ef
Parth Rohilla <parth05rohilla@gmail.com>
2022-10-08 19:05:23 +0530

    Time: 1297 ms (88.80%), Space: 167.3 MB (14.78%) - LeetHub

--- 1743-restore-the-array-from-adjacent-pairs/1743-restore-the-array-from-adjacent-pairs.py
+++ 1743-restore-the-array-from-adjacent-pairs/1743-restore-the-array-from-adjacent-pairs.py
@@ -1,24 +1,22 @@
 class Solution:
     def restoreArray(self, adjacentPairs: List[List[int]]) -> List[int]:
+        adj, self.ans, seen, start = defaultdict(list), [], set(), 0
+        # DFS Helper for graph traversal
         def dfs(i):
             self.ans.append(i)
             seen.add(i)
             for nei in adj[i]:
-                if nei not in seen:
+                if nei not in seen: 
                     dfs(nei)
             return
-        
-        adj = defaultdict(list)
+        # Create Adjacency List
         for a,b in adjacentPairs:
             adj[a].append(b)
             adj[b].append(a)
-        
-        self.ans = []
-        seen = set()
-        start = 0
+        # Find the degree 1 node - starting point for DFS
         for x in adj:
             if len(adj[x]) == 1:
-                start = x
-        
+                start = x        
+        # Call DFS to populate path/ans
         dfs(start)
         return self.ans
