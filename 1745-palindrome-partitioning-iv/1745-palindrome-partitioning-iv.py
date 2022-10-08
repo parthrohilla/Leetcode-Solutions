591ee64d99c8bcd4dff0754c3b7535c6c449957e
Parth Rohilla <parth05rohilla@gmail.com>
2022-10-08 15:24:54 +0530

    Time: 5410 ms (54.54%), Space: 18.6 MB (78.79%) - LeetHub

--- 1745-palindrome-partitioning-iv/1745-palindrome-partitioning-iv.py
+++ 1745-palindrome-partitioning-iv/1745-palindrome-partitioning-iv.py
@@ -1,10 +1,15 @@
 class Solution:
     def checkPartitioning(self, s: str) -> bool:
-        @lru_cache(None)
+        memo = {}
         def dfs(substring, partitions):
             if partitions == 0: return substring == substring[::-1]
+            if (substring, partitions) in memo: return memo[(substring, partitions)]
+            
+            ans = False
             for i in range(1,len(substring)):
-                if substring[:i] == substring[:i][::-1] and dfs(substring[i:], partitions - 1):
-                    return True
+                if substring[:i] == substring[:i][::-1] and dfs(substring[i:], partitions - 1): 
+                    ans= True
+            memo[(substring, partitions)] = ans
+            return ans
         return dfs(s,2)
     
