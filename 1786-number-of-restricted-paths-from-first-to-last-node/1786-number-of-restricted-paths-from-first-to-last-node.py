62f36118cfdf60ea1d8df24c7b1b7281141241c1
Parth Rohilla <parth05rohilla@gmail.com>
2022-10-10 04:22:25 +0530

    Time: 5661 ms (8.47%), Space: 68 MB (31.86%) - LeetHub

--- 1786-number-of-restricted-paths-from-first-to-last-node/1786-number-of-restricted-paths-from-first-to-last-node.py
+++ 1786-number-of-restricted-paths-from-first-to-last-node/1786-number-of-restricted-paths-from-first-to-last-node.py
@@ -1,38 +1,34 @@
 class Solution:
     def countRestrictedPaths(self, n: int, edges: List[List[int]]) -> int:
-        d = [math.inf]*n
-        visited = set()
-        heap = [[0,n-1]]
-        adj = defaultdict(list)
-        
+        d, visited, adj, mod = [math.inf]*n, set(), defaultdict(list), (10**9) + 7
         for u,v,w in edges:
             adj[u-1].append([v-1,w])
             adj[v-1].append([u-1,w])
         
+        heap = [[0,n-1]]
         while heap:
             distance, node = heapq.heappop(heap)
-            if node in visited:
-                continue
-            
-            visited.add(node)
-            d[node] = distance
-            
-            for nei, weight in adj[node]:
-                if nei not in visited:
-                    heapq.heappush(heap,[distance+weight, nei])
-        
-        mod = (10**9) + 7
-        visited = set()
+            if node not in visited:
+                visited.add(node)
+                d[node] = distance
+
+                for nei, weight in adj[node]:
+                    if nei not in visited:
+                        heapq.heappush(heap,[distance+weight, nei])
+                        
+        path = set()
         @lru_cache(None)
         def dfs(i):
             if i == 0: return 1
-            visited.add(i)
+            path.add(i)
+            
             count = 0
             for nei, wei in adj[i]:
-                if nei not in visited:
+                if nei not in path:
                     if d[nei] > d[i]:
                         count += dfs(nei) % mod
-            visited.remove(i)
+            
+            path.remove(i)
             return count % mod
         
         return dfs(n-1)
