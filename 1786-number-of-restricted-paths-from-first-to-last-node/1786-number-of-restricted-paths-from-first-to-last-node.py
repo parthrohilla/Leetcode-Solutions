800bbf094c02e89e642760204e53798cea50046f
Parth Rohilla <parth05rohilla@gmail.com>
2022-10-10 04:30:47 +0530

    Time: 2464 ms (87.10%), Space: 66.9 MB (32.66%) - LeetHub

--- 1786-number-of-restricted-paths-from-first-to-last-node/1786-number-of-restricted-paths-from-first-to-last-node.py
+++ 1786-number-of-restricted-paths-from-first-to-last-node/1786-number-of-restricted-paths-from-first-to-last-node.py
@@ -1,10 +1,11 @@
 class Solution:
     def countRestrictedPaths(self, n: int, edges: List[List[int]]) -> int:
         d, visited, adj, mod = [math.inf]*n, set(), defaultdict(list), (10**9) + 7
+        # Creaste Adjacency List
         for u,v,w in edges:
             adj[u-1].append([v-1,w])
             adj[v-1].append([u-1,w])
-        
+        # Djisktra's Algorith for shortest path
         heap = [[0,n-1]]
         while heap:
             distance, node = heapq.heappop(heap)
@@ -16,19 +17,15 @@ class Solution:
                     if nei not in visited:
                         heapq.heappush(heap,[distance+weight, nei])
                         
-        path = set()
+        # DFS for finding the increasing distance path, can be seen as LIS on a Graph
         @lru_cache(None)
         def dfs(i):
             if i == 0: return 1
-            path.add(i)
-            
             count = 0
             for nei, wei in adj[i]:
                 if nei not in path:
                     if d[nei] > d[i]:
-                        count += dfs(nei) % mod
-            
-            path.remove(i)
-            return count % mod
+                        count =(count + dfs(nei)) % mod
+            return count
         
         return dfs(n-1)
