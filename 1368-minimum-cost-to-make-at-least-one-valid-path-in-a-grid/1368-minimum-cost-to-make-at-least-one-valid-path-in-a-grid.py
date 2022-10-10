ff11814df9716b8d8b44a5e20ebb68ab62780ced
Parth Rohilla <parth05rohilla@gmail.com>
2022-10-10 16:49:08 +0530

    Time: 3840 ms (6.64%), Space: 14.4 MB (100.00%) - LeetHub

--- 1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid.py
+++ 1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid.py
@@ -1,23 +1,21 @@
 class Solution:
     def minCost(self, graph: List[List[int]]) -> int:
-        m, n = len(graph), len(graph[0])
-        dp = [[math.inf]*n for _ in range(m)]
+        m, n, q = len(graph), len(graph[0]), deque()
+        dp, directions = [[math.inf]*n for _ in range(m)], [[0,1], [0,-1], [1,0], [-1,0]]
         
-        directions = [[0,1], [0,-1], [1,0], [-1,0]]
-        q = deque()
         q.append((0,0))
         dp[0][0] = 0
         while q:
             x,y = q.popleft()
             arrow = graph[x][y]
             for i in range(4):
-                dx, dy = directions[i][0], directions[i][1]
-                if 0<=x + dx<m and 0<=y + dy<n:
-                    if i + 1 == arrow and dp[x+dx][y+dy] > dp[x][y]:
-                        dp[x+dx][y+dy] = dp[x][y]
-                        q.append((x+dx, y+dy))
-                    elif dp[x][y] + 1 < dp[x+dx][y+dy]:
-                        dp[x+dx][y+dy] = 1 + dp[x][y]
-                        q.append((x+dx, y+dy))
+                new_x, new_y = x+directions[i][0], y+directions[i][1]
+                if 0 <= new_x < m and 0 <= new_y < n:
+                    if i + 1 == arrow and dp[new_x][new_y] > dp[x][y]:
+                        dp[new_x][new_y] = dp[x][y]
+                        q.append((new_x, new_y))
+                    elif dp[x][y] + 1 < dp[new_x][new_y]:
+                        dp[new_x][new_y] = 1 + dp[x][y]
+                        q.append((new_x, new_y))
         return dp[m-1][n-1]
             
