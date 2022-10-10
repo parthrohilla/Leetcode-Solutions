4534b8bacea03c3a6fee006cadf73158a5aa0cee
Parth Rohilla <parth05rohilla@gmail.com>
2022-10-10 18:55:05 +0530

    Time: 7261 ms (12.89%), Space: 181.9 MB (5.39%) - LeetHub

--- 2328-number-of-increasing-paths-in-a-grid/2328-number-of-increasing-paths-in-a-grid.py
+++ 2328-number-of-increasing-paths-in-a-grid/2328-number-of-increasing-paths-in-a-grid.py
@@ -1,11 +1,7 @@
 class Solution:
     def countPaths(self, grid: List[List[int]]) -> int:
-        m, n = len(grid), len(grid[0])
-        directions = [[0,1], [0,-1], [1,0], [-1,0]]
-        mod = (10**9) + 7
-        
-        def isValid(i,j):
-            return 0<= i <m and 0<= j <n
+        m, n, mod = len(grid), len(grid[0]), (10**9) + 7
+        directions, ans = [[0,1], [0,-1], [1,0], [-1,0]], n*m
         
         @lru_cache(None)
         def dfs(i,j):
@@ -13,14 +9,16 @@ class Solution:
             count = 0
             for dx, dy in directions:
                 x,y = i + dx, j + dy
-                if isValid(x,y) and grid[x][y] > grid[i][j]:
-                    count += (1 + dfs(x,y))
+                if self.isValid(x, y, m, n) and grid[x][y] > grid[i][j]:
+                    count += (1 + dfs(x, y))
             return count % mod
         
-        ans = n*m
         for i in range(m):
             for j in range(n):
                 ans += dfs(i,j)
         return ans % mod
+    
+    def isValid(self, i, j, m, n):
+        return 0<= i <m and 0<= j <n
         
                 
