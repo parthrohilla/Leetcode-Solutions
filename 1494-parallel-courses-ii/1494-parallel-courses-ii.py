2119bc4b853f083c51597e4dc9f89991800a685b
Parth Rohilla <parth05rohilla@gmail.com>
2022-10-12 18:19:31 +0530

    Time: 3302 ms (29.00%), Space: 21.5 MB (15.27%) - LeetHub

--- 1494-parallel-courses-ii/1494-parallel-courses-ii.py
+++ 1494-parallel-courses-ii/1494-parallel-courses-ii.py
@@ -1,29 +1,23 @@
 class Solution:
     def minNumberOfSemesters(self, n: int, relations: List[List[int]], k: int) -> int:
-        self.k = k
-        self.n = n
-        self.adj = defaultdict(list)
-        indegrees = [0]*n
+        self.k, self.n, self.adj = k, n, defaultdict(list)
+        indegrees = [0] * self.n
         for u,v in relations:
             self.adj[u-1].append(v-1)
             indegrees[v-1] += 1
-        
-        return self.dfs((1 << self.n) - 1, tuple(indegrees))
+        mask = (1 << self.n) - 1 #1(set-bit) means NOT-Taken, so initially all 0 to (n-1) nodes are 1 
+        return self.dfs(mask, tuple(indegrees))
     
     @lru_cache(None)
     def dfs(self, bitmask, indegrees):
         if not bitmask: return 0
-        
         nodes = [x for x in range(self.n) if bitmask & (1 << x) and indegrees[x] == 0]
         ans = math.inf
-        for courses in combinations(nodes, min(self.k, len(nodes))):
-            new_bitmask = bitmask
-            new_indegrees = list(indegrees)
-            
-            for c in courses:
+        for courses_taken in combinations(nodes, min(self.k, len(nodes))):
+            new_bitmask, new_indegrees = bitmask, list(indegrees)
+            for c in courses_taken:
                 new_bitmask ^= (1<<c)
                 for nei in self.adj[c]:
-                    new_indegrees[nei] -= 1
-                    
+                    new_indegrees[nei] -= 1        
             ans = min(ans, 1 + self.dfs(new_bitmask, tuple(new_indegrees)))
         return ans
