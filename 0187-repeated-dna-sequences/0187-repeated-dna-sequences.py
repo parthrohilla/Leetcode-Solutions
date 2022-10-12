2225430dc982f332c796f05c6aa4b01a975a7113
Parth Rohilla <parth05rohilla@gmail.com>
2022-10-12 19:52:32 +0530

    Time: 151 ms (33.83%), Space: 27.6 MB (38.10%) - LeetHub

--- 0187-repeated-dna-sequences/0187-repeated-dna-sequences.py
+++ 0187-repeated-dna-sequences/0187-repeated-dna-sequences.py
@@ -3,9 +3,9 @@ class Solution:
         hashset = defaultdict(int)
         for i in range(len(s)-9):
             hashset[s[i:i+10]] += 1
+        
         count = max(hashset.values()) if hashset else 0
         ans = []
         for key in hashset:
-            if count > 1 and hashset[key] == count:
-                ans.append(key)
+            if count > 1 and hashset[key] == count: ans.append(key)
         return ans
