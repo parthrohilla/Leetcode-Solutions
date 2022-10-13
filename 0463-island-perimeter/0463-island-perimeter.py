dac414bdf090a4839026acfa23b8e0a6e05b9ed8
Parth Rohilla <parth05rohilla@gmail.com>
2022-10-13 15:57:37 +0530

    Time: 621 ms (88.29%), Space: 28.9 MB (8.39%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.py
+++ 0463-island-perimeter/0463-island-perimeter.py
@@ -3,14 +3,10 @@ class Solution:
         m,n,visited = len(grid), len(grid[0]), set()
         
         def dfs(i,j):
-            if (i,j) in visited: 
-                return 0
-            if i<0 or i>=m or j<0 or j>=n or grid[i][j] == 0: 
-                return 1
+            if (i,j) in visited: return 0
+            if i<0 or i>=m or j<0 or j>=n or grid[i][j] == 0: return 1
             visited.add((i,j))
-            perimeter = dfs(i+1,j) + dfs(i-1,j) + dfs(i,j+1) + dfs(i,j-1)
-            return perimeter
-        
+            return dfs(i+1,j) + dfs(i-1,j) + dfs(i,j+1) + dfs(i,j-1)
         
         for i in range(m):
             for j in range(n):
