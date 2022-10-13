b5eafeed6747f87520bd351ec07aa9bfba146115
Parth Rohilla <parth05rohilla@gmail.com>
2022-10-13 17:34:43 +0530

    Time: 817 ms (37.92%), Space: 14.3 MB (33.71%) - LeetHub

--- 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.py
+++ 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.py
@@ -1,7 +1,6 @@
 class Solution:
     def findSubstring(self, s: str, words: List[str]) -> List[int]:
-        freq, k, ans = dict(Counter(words)), len(words[0]), []
-        
+        freq, k, ans = Counter(words), len(words[0]), []
         for i in range(len(s)- k*len(words)+1):
             copyMap = freq.copy()
             for j in range(i, i + k*len(words), k):
@@ -10,6 +9,5 @@ class Solution:
                     if copyMap[w] == 1: del copyMap[w]
                     else: copyMap[w] -= 1
                     if not copyMap: ans.append(i)
-                else:
-                    break    
+                else: break    
         return ans
