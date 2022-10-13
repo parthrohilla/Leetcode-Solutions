b3b1ce70eed417d70afe35e9d6bc73a459fa235a
Parth Rohilla <parth05rohilla@gmail.com>
2022-10-13 15:54:06 +0530

    Time: 1678 ms (14.95%), Space: 28.8 MB (8.39%) - LeetHub

---
+++ 0463-island-perimeter/0463-island-perimeter.py
@@ -0,0 +1,19 @@
+class Solution:
+    def islandPerimeter(self, grid: List[List[int]]) -> int:
+        m,n,visited = len(grid), len(grid[0]), set()
+        
+        def dfs(i,j):
+            if (i,j) in visited: 
+                return 0
+            if i<0 or i>=m or j<0 or j>=n or grid[i][j] == 0: 
+                return 1
+            visited.add((i,j))
+            perimeter = dfs(i+1,j) + dfs(i-1,j) + dfs(i,j+1) + dfs(i,j-1)
+            return perimeter
+        
+        
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == 1:
+                    return dfs(i,j)
+        return 0
