d4e0d9c2518ea9c18f1438562177088fc54575d6
Parth Rohilla <parth05rohilla@gmail.com>
2022-10-14 20:14:31 +0530

    Time: 3361 ms (24.30%), Space: 17.7 MB (63.73%) - LeetHub

---
+++ 1462-course-schedule-iv/1462-course-schedule-iv.py
@@ -0,0 +1,17 @@
+class Solution:
+    def checkIfPrerequisite(self, n: int, prerequisites: List[List[int]], queries: List[List[int]]) -> List[bool]:
+        adj = [[False]*n for _ in range(n)]
+        
+        for u,v in prerequisites:
+            adj[u][v] = True
+            
+        for k in range(n):
+            for i in range(n):
+                for j in range(n):
+                    adj[i][j] |= adj[i][k] and adj[k][j]
+        
+        ans = []
+        for u,v in queries:
+            ans.append(adj[u][v])
+        return ans
+    
