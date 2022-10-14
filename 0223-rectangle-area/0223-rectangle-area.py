97f1c0af350eb33243b77c2c31e79f20ed67d447
Parth Rohilla <parth05rohilla@gmail.com>
2022-10-14 13:27:56 +0530

    Time: 94 ms (61.03%), Space: 13.9 MB (70.19%) - LeetHub

--- 0223-rectangle-area/0223-rectangle-area.py
+++ 0223-rectangle-area/0223-rectangle-area.py
@@ -1,7 +1,9 @@
 class Solution:
     def computeArea(self, A: int, B: int, C: int, D: int, E: int, F: int, G: int, H: int) -> int:
-        left = max(A,E)
-        right = max(min(C,G), left)
-        bottom = max(B,F)
-        top = max(min(D,H), bottom)
-        return (C-A)*(D-B) - (right-left)*(top-bottom) + (G-E)*(H-F);
+        left, bottom = max(A,E), max(B,F)
+        right, top = max(min(C,G), left), max(min(D,H), bottom)
+        
+        rect1 = (C-A)*(D-B)
+        rect2 = (G-E)*(H-F)
+        common_rect = (right-left)*(top-bottom)
+        return rect1 + rect2 - common_rect
