49865cd20ea12a033b48339cf62d32b93a637d2e
Parth Rohilla <parth05rohilla@gmail.com>
2022-10-15 03:33:15 +0530

    Time: 2569 ms (23.04%), Space: 17.1 MB (67.24%) - LeetHub

--- 2115-find-all-possible-recipes-from-given-supplies/2115-find-all-possible-recipes-from-given-supplies.py
+++ 2115-find-all-possible-recipes-from-given-supplies/2115-find-all-possible-recipes-from-given-supplies.py
@@ -2,22 +2,21 @@ class Solution:
     def findAllRecipes(self, recipes: List[str], ingredients: List[List[str]], supplies: List[str]) -> List[str]:
         adj = defaultdict(list)
         indegrees = defaultdict(int)
-        
+        # Make a Graph such that all ingredients lead us to the recipe
         for i in range(len(recipes)):
-            for ingr in ingredients[i]:
-                adj[ingr].append(recipes[i])
+            for ingredient in ingredients[i]:
+                adj[ingredient].append(recipes[i])
                 indegrees[recipes[i]] += 1
-        
-        ans = []
-        q = deque()
+        # Add supplies to the queue, as we would use the supplies and remove all the things we can make with that ingredient
+        ans, q = [], deque()
         for supply in supplies:
             q.append(supply)
         
         while q:
-            sup = q.popleft()
-            for k in adj[sup]:
-                indegrees[k] -= 1
-                if indegrees[k] == 0: 
-                    q.append(k)
-                    ans.append(k)
+            ingredient = q.popleft()
+            for product in adj[ingredient]:
+                indegrees[product] -= 1
+                if indegrees[product] == 0: 
+                    q.append(product)
+                    ans.append(product)
         return ans
