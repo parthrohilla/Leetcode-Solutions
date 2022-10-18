bc9596aff89ca852010951b7944ef537ca01ef91
Parth Rohilla <parth05rohilla@gmail.com>
2022-10-18 15:34:39 +0530

    Time: 65 ms (75.56%), Space: 14 MB (28.81%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.py
+++ 0038-count-and-say/0038-count-and-say.py
@@ -5,10 +5,9 @@ class Solution:
             prev = helper(x-1)
             count, res = 1, ""
             for i in range(len(prev)):
-                if i + 1 < len(prev) and prev[i] == prev[i+1]: count += 1
+                if (i + 1) < len(prev) and prev[i] == prev[i+1]: count += 1
                 else:
                     res += str(count) + str(prev[i])
                     count = 1
             return res
-        
         return helper(n)
