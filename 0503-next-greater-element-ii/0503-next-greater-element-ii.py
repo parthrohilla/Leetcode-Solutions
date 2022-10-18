4e759ab071c3bbaac9d740abf407ea7086f6d7c2
Parth Rohilla <parth05rohilla@gmail.com>
2022-10-18 22:11:01 +0530

    Time: 210 ms (96.95%), Space: 16.3 MB (9.70%) - LeetHub

---
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.py
@@ -0,0 +1,18 @@
+class Solution:
+    def nextGreaterElements(self, nums: List[int]) -> List[int]:
+        hashmap = {}
+        for i, num in enumerate(nums):
+            hashmap[num] = i
+        
+        stack = []
+        ans = [-1] * len(nums)
+        
+        index = 0
+        n = len(nums)
+        temp = nums + nums
+        for i, num in enumerate(temp):
+            while stack and temp[stack[-1]] < num:
+                pop = stack.pop()
+                ans[pop] = num
+            stack.append(i%n)
+        return ans
