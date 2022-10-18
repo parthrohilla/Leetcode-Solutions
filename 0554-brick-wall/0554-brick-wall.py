4b0133c748d64786dff9f3861c830d63dbade16e
Parth Rohilla <parth05rohilla@gmail.com>
2022-10-18 14:09:36 +0530

    Time: 230 ms (79.82%), Space: 19 MB (67.46%) - LeetHub

--- 0554-brick-wall/0554-brick-wall.py
+++ 0554-brick-wall/0554-brick-wall.py
@@ -1,12 +1,11 @@
 class Solution:
     def leastBricks(self, wall: List[List[int]]) -> int:
-        corner = {}
-        rows = len(wall)
+        corner, rows = {}, len(wall)
         for i in range(rows):
             prefix = 0
             for k in wall[i][:-1]:
                 prefix += k
                 corner[prefix] = 1 + corner.get(prefix, 0)
         
-        temp = max(corner.values()) if corner else 0
-        return rows - temp
+        max_brick_edges = max(corner.values()) if corner else 0
+        return rows - max_brick_edges
