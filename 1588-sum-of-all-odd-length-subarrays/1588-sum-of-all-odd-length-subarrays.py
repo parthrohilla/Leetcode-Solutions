6cd4fcdb0a6677b9c6bc33aa5ba7eef4de0b9686
Parth Rohilla <parth05rohilla@gmail.com>
2022-10-19 20:23:38 +0530

    Time: 39 ms (97.78%), Space: 14 MB (21.65%) - LeetHub

--- 1588-sum-of-all-odd-length-subarrays/1588-sum-of-all-odd-length-subarrays.py
+++ 1588-sum-of-all-odd-length-subarrays/1588-sum-of-all-odd-length-subarrays.py
@@ -2,20 +2,27 @@ class Solution:
     def sumOddLengthSubarrays(self, arr: List[int]) -> int:
         self.s = 0
         n = len(arr)
+        # brute Force Solution O(n^2)
+        def brute_force():
+            for i in range(n):
+                 for j in range(i, n, 2):
+                    self.s += sum(arr[i:j+1])
+            return self.s
         
-        # def brute_force():
-        #     #------ Brute - Force Solution ------
-        #     for i in range(n):
-        #          for j in range(i, n, 2):
-        #             self.s += sum(arr[i:j+1])
-        #     return self.s
-        # return brute_force()
+        # optimal Solution - Counting the sub-arrays the current element is part of
+        def optimal():
+            for i, num in enumerate(arr):
+                T = (i+1)*(n-i)
+                odd_sub_arrays = (T+1) // 2
+                self.s += num * odd_sub_arrays
+            return self.s
         
-        for i, num in enumerate(arr):
-            T = (i+1)*(n-i)
-            odd_sub_arrays = (T+1) // 2
-            self.s += num * odd_sub_arrays
-        return self.s
+        #return brute_force()
+        return optimal()
+        
+        
+    
+    
         
         
         
