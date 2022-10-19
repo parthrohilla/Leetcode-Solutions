6a2a0143c449cba0e9308d5217ba4f3134f5aceb
Parth Rohilla <parth05rohilla@gmail.com>
2022-10-19 15:07:35 +0530

    Time: 134 ms (19.73%), Space: 13.9 MB (64.49%) - LeetHub

---
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.py
@@ -0,0 +1,22 @@
+class Solution:
+    def topKFrequent(self, words: List[str], k: int) -> List[str]:
+        word_map = {}
+        for word in words:
+            word_map[word] = 1 + word_map.get(word, 0)
+            
+        bucket = [[] for _ in range(len(words)+1)]
+        for key, value in word_map.items():
+            bucket[value].append(key)
+            
+        res = []
+        for i in range(len(bucket)-1, -1, -1):
+            if len(bucket[i]) != 0:
+                bucket[i].sort()
+                for word in bucket[i]:
+                    if len(res) == k:
+                        return res
+                    else:
+                        res.append(word)
+                    
+        return res
+        
