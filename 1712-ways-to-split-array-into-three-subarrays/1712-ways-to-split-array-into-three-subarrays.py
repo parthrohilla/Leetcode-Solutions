ac50e98e4d4668be9950981cc8193580d4e3a14f
Parth Rohilla <parth05rohilla@gmail.com>
2022-10-20 13:52:45 +0530

    Time: 1806 ms (80.42%), Space: 27.6 MB (22.98%) - LeetHub

---
+++ 1712-ways-to-split-array-into-three-subarrays/1712-ways-to-split-array-into-three-subarrays.py
@@ -0,0 +1,14 @@
+class Solution:
+    def waysToSplit(self, nums: List[int]) -> int:
+        prefix = [0]
+        for x in nums: prefix.append(prefix[-1] + x)
+        
+        ans = j = k = 0 
+        for i in range(1, len(nums)): 
+            j = max(j, i+1)
+            while j < len(nums) and 2*prefix[i] > prefix[j]: j += 1
+            k = max(k, j)
+            while k < len(nums) and 2*prefix[k] <= prefix[i] + prefix[-1]: k += 1
+            ans += k - j 
+        return ans % 1_000_000_007
+        
