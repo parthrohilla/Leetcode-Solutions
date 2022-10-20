083f7f97b6c3b04c5c13584beb41c7b50d5f6e5b
Parth Rohilla <parth05rohilla@gmail.com>
2022-10-20 21:21:37 +0530

    Time: 1124 ms (76.51%), Space: 19.1 MB (98.11%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.py
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.py
@@ -1,17 +1,19 @@
 class Solution:
     def numberOfSubarrays(self, nums: List[int], k: int) -> int:
+        # Change odd Numbers to 1 ans Even Numbers to 0
         for i, num in enumerate(nums):
             if num % 2: nums[i] = 1
             else: nums[i] = 0
         
+        # The problem now becomes subarray Sum Equals K 
         hashmap = {}
-        prefix = 0
+        ans = prefix = 0
         hashmap[prefix] = 1
-        ans = 0
         for i, num in enumerate(nums):
             prefix += num
             if (prefix - k) in hashmap:
                 ans += hashmap[prefix-k]
             hashmap[prefix] = 1 + hashmap.get(prefix, 0)
+        
         return ans
         
