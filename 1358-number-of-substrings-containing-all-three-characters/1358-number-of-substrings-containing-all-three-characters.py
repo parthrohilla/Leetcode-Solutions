668e4bac0d42e79aee26c562dc5774a40b3aa0b1
Parth Rohilla <parth05rohilla@gmail.com>
2022-10-20 19:41:06 +0530

    Time: 407 ms (65.89%), Space: 14.5 MB (9.19%) - LeetHub

--- 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.py
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.py
@@ -5,18 +5,20 @@ class Solution:
         seen = {}
         ans = 0
         while r < n:
+            # Acquire characters
             seen[s[r]] = 1 + seen.get(s[r], 0)
+            
             if len(seen.keys()) == 3:
                 ans += (n - r)
-            
-            while len(seen.keys()) == 3:
-                char = s[l]
-                seen[char] -= 1
-                if seen[char] == 0:
-                    del seen[char]  
-                else:
-                    ans += (n - r)
-                l += 1
+                
+                while len(seen.keys()) == 3:
+                    char = s[l]
+                    seen[char] -= 1
+                    if seen[char] == 0:
+                        del seen[char]  
+                    else:
+                        ans += (n - r)
+                    l += 1
             
             r += 1
         return ans
