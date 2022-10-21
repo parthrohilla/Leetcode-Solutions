38ee4fac6c4fe43f1ce94f6565095ee2f547e00c
Parth Rohilla <parth05rohilla@gmail.com>
2022-10-21 17:01:42 +0530

    Time: 233 ms (88.45%), Space: 15.2 MB (56.72%) - LeetHub

--- 0581-shortest-unsorted-continuous-subarray/0581-shortest-unsorted-continuous-subarray.py
+++ 0581-shortest-unsorted-continuous-subarray/0581-shortest-unsorted-continuous-subarray.py
@@ -5,6 +5,5 @@ class Solution:
         r = len(nums)-1
         while l < len(nums) and nums[l] == temp[l]: l += 1
         while r>=0 and nums[r] == temp[r]: r -= 1
-        
         return 0 if l == len(nums) else (r-l+1)
             
