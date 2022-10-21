38ce02ba454810c57df2df95e3424da0825c7b0d
Parth Rohilla <parth05rohilla@gmail.com>
2022-10-21 18:12:46 +0530

    Time: 795 ms (5.08%), Space: 15.3 MB (56.72%) - LeetHub

--- 0581-shortest-unsorted-continuous-subarray/0581-shortest-unsorted-continuous-subarray.py
+++ 0581-shortest-unsorted-continuous-subarray/0581-shortest-unsorted-continuous-subarray.py
@@ -8,24 +8,25 @@ class Solution:
         # return 0 if l == len(nums) else (r-l+1)
         if len(nums) <= 1: return 0
         
-        mini = math.inf
-        maxi = -math.inf
+        def checkMinMax(mini, maxi, num):
+            self.mini = min(mini, num)
+            self.maxi = max(maxi, num)
+        
+        self.mini = math.inf
+        self.maxi = -math.inf
         for i, num in enumerate(nums):
             if i == 0 and num > nums[i+1]:
-                mini = min(mini, num)
-                maxi = max(maxi, num)
+                checkMinMax(self.mini, self.maxi, num)
             elif i == len(nums) - 1 and num < nums[i-1]:
-                mini = min(mini, num)
-                maxi = max(maxi, num)        
+                checkMinMax(self.mini, self.maxi, num)       
             elif 0 < i < len(nums)-1:
                 if num < nums[i-1] or num > nums[i+1]:
-                    mini = min(mini, num)
-                    maxi = max(maxi, num)
+                    checkMinMax(self.mini, self.maxi, num)
         
         left = 0
         right = len(nums)-1
-        while left < len(nums) and nums[left] <= mini: left += 1
-        while right >= 0 and nums[right] >= maxi: right -= 1
+        while left < len(nums) and nums[left] <= self.mini: left += 1
+        while right >= 0 and nums[right] >= self.maxi: right -= 1
         
         ans = right - left + 1
         return ans if ans > 0 else 0
