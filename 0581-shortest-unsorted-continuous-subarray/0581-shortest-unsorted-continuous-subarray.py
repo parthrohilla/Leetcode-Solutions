8ff8c5d12666f3892ee9c843de79dfe6d17ba6fd
Parth Rohilla <parth05rohilla@gmail.com>
2022-10-21 17:01:26 +0530

    Time: 499 ms (39.34%), Space: 15.3 MB (56.72%) - LeetHub

--- 0581-shortest-unsorted-continuous-subarray/0581-shortest-unsorted-continuous-subarray.py
+++ 0581-shortest-unsorted-continuous-subarray/0581-shortest-unsorted-continuous-subarray.py
@@ -3,11 +3,8 @@ class Solution:
         temp = sorted(nums)
         l = 0
         r = len(nums)-1
-        while l < len(nums) and nums[l] == temp[l]:
-            l += 1
-        while r>=0 and nums[r] == temp[r]:
-            r -= 1
+        while l < len(nums) and nums[l] == temp[l]: l += 1
+        while r>=0 and nums[r] == temp[r]: r -= 1
         
-        if l == len(nums): return 0
-        return (r-l+1)
+        return 0 if l == len(nums) else (r-l+1)
             
