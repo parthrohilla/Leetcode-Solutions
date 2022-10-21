9e7b54b138b82761b74ef71969be0a1def8e4545
Parth Rohilla <parth05rohilla@gmail.com>
2022-10-21 15:50:06 +0530

    Time: 3859 ms (24.61%), Space: 27.7 MB (93.38%) - LeetHub

--- 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.py
+++ 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.py
@@ -4,7 +4,7 @@ class Solution:
         l = ans = prefix =  0
         for r, num in enumerate(nums):
             prefix += num
-            while num*(r-l+1) > prefix + k:
+            while num * (r-l+1) > prefix + k:
                 prefix -= nums[l]
                 l += 1
             ans = max(ans, r-l+1)
