50cca449792d75d317db513b4a960a5348934b65
Parth Rohilla <parth05rohilla@gmail.com>
2022-10-25 00:51:53 +0530

    Time: 39 ms (83.33%), Space: 13.8 MB (33.33%) - LeetHub

---
+++ 2446-determine-if-two-events-have-conflict/2446-determine-if-two-events-have-conflict.py
@@ -0,0 +1,18 @@
+class Solution:
+    def haveConflict(self, event1: List[str], event2: List[str]) -> bool:
+        
+        def convert(event):
+            return int(event[:2])*60 + int(event[-2:])
+        
+        e1_start = convert(event1[0])
+        e1_end = convert(event1[1])
+        e2_start = convert(event2[0])
+        e2_end = convert(event2[1])
+        
+        
+        if e1_start < e2_start and e1_end >= e2_start:
+            return True
+        elif e2_start < e1_start and e2_end >= e1_start:
+            return True
+        else:
+            return False
