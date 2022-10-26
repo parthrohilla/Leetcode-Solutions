11af010e9dfedf208010064135d81e557f4b3132
Parth Rohilla <parth05rohilla@gmail.com>
2022-10-26 14:59:51 +0530

    Time: 1028 ms (97.05%), Space: 33.2 MB (73.31%) - LeetHub

---
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.py
@@ -0,0 +1,23 @@
+class Solution:
+    def checkSubarraySum(self, nums: List[int], k: int) -> bool:
+        if len(nums) < 2: return False
+        if k == 1: return True
+        seen = {}
+        seen[0] = -1
+        prefix = 0
+        for i, num in enumerate(nums):
+            prefix += num
+            
+            if num == 0:
+                if i >= 1 and nums[i-1] == 0: return True
+                else: continue
+            
+            m = 1
+            while m*k <= prefix:
+                needed = prefix - m * k
+                if needed in seen and i - seen[needed] >= 2:
+                    return True
+                else: m += 1
+            seen[prefix] = i
+        return False
+            
