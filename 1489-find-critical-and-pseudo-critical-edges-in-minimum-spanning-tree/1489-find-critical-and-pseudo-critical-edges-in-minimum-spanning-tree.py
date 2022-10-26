b48c6fe1f3ed73598438115a3b43120c7daf812f
Parth Rohilla <parth05rohilla@gmail.com>
2022-10-27 00:11:29 +0530

    Time: 4295 ms (21.69%), Space: 14.2 MB (33.73%) - LeetHub

--- 1489-find-critical-and-pseudo-critical-edges-in-minimum-spanning-tree/1489-find-critical-and-pseudo-critical-edges-in-minimum-spanning-tree.py
+++ 1489-find-critical-and-pseudo-critical-edges-in-minimum-spanning-tree/1489-find-critical-and-pseudo-critical-edges-in-minimum-spanning-tree.py
@@ -15,44 +15,49 @@ class UnionFind:
 
 class Solution:
     def findCriticalAndPseudoCriticalEdges(self, n: int, edges: List[List[int]]) -> List[List[int]]:
-        edges = [(u, v, w, i) for i, (u, v, w) in enumerate(edges)]
-        edges.sort(key = lambda x : x[2])
+        self.n = n
+        self.edges = [(u, v, w, i) for i, (u, v, w) in enumerate(edges)]
+        self.edges.sort(key = lambda x : x[2])
         
-        def exclude_edge(exclude):
-            uf = UnionFind(n)
-            ans = 0
-            for i, (u, v, w, _) in enumerate(edges):
-                if i == exclude: continue
-                if uf.union(u, v): 
-                    ans += w
-            p = uf.find(0)
-            for node in range(n):
-                if uf.find(node) != p: return math.inf
-            return ans
-             
-        def include_edge(include):
-            uf = UnionFind(n)
-            u0, v0, w0, _ = edges[include]
-            ans = w0
-            uf.union(u0, v0)
-            
-            for i, (u, v, w, _) in enumerate(edges):
-                if uf.union(u, v):
-                    ans += w
-            
-            p = uf.find(0)
-            for node in range(n):
-                if uf.find(node) != p: return math.inf
-            return ans
-        
-        base = exclude_edge(-1)
+        base = self.exclude_edge(-1)
         critical, pseudoCritical = set(), set()
         for i in range(len(edges)):
-            if exclude_edge(i) > base:
-                critical.add(edges[i][3])
+            if self.exclude_edge(i) > base:
+                critical.add(self.edges[i][3])
             else:
-                if include_edge(i) == base:
-                    pseudoCritical.add(edges[i][3])
+                if self.include_edge(i) == base:
+                    pseudoCritical.add(self.edges[i][3])
                     
         return [list(critical), list(pseudoCritical)]
+           
+                         
+    def include_edge(self, include):
+        uf = UnionFind(self.n)
+        u0, v0, w0, _ = self.edges[include]
+        ans = w0
+        uf.union(u0, v0)
+            
+        for i, (u, v, w, _) in enumerate(self.edges):
+            if uf.union(u, v): ans += w
+            
+        p = uf.find(0)
+        for node in range(self.n):
+            if uf.find(node) != p: 
+                return math.inf
+        
+        return ans
+    
+    def exclude_edge(self, exclude):
+        uf = UnionFind(self.n)
+        ans = 0
+        for i, (u, v, w, _) in enumerate(self.edges):
+            if i == exclude: continue
+            if uf.union(u, v): ans += w
+        
+        p = uf.find(0)
+        for node in range(self.n):
+            if uf.find(node) != p: 
+                return math.inf
+        
+        return ans
         
