62565f0fe0213d6811824d257368eb705f675b14
Parth Rohilla <parth05rohilla@gmail.com>
2022-10-28 17:13:31 +0530

    Time: 664 ms (41.61%), Space: 16 MB (51.01%) - LeetHub

--- 1311-get-watched-videos-by-your-friends/1311-get-watched-videos-by-your-friends.py
+++ 1311-get-watched-videos-by-your-friends/1311-get-watched-videos-by-your-friends.py
@@ -1,9 +1,9 @@
 class Solution:
     def watchedVideosByFriends(self, watchedVideos: List[List[str]], friends: List[List[int]], id: int, level: int) -> List[str]:
         n, q = len(watchedVideos), deque([id])
-        
         freq, visited = {}, set([id])
         current_level = 0
+        
         # BFS for reaching the "level" of required friends
         while q and current_level < level:
             k = len(q)
@@ -20,17 +20,6 @@ class Solution:
             node = q.popleft()
             for vid in watchedVideos[node]:
                 freq[vid] = 1 + freq.get(vid, 0)
-        
-        # Create a list of [frequqncy_watched, video] such that it can be sorted by frequency as required by LC Judge
-        temp = []
-        for v,f in freq.items():
-            temp.append([f, v])
-        temp.sort()
-        
-        # Returning videos by least -> largest frequency
-        ans = []
-        for _,v in temp:
-            ans.append(v)
         
         return sorted(freq.keys(), key = lambda x : (freq[x],x))
     
