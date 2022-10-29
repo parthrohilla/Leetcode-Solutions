49f519b5a63d6cfa0b9182b0083c0d191603bc5f
Parth Rohilla <parth05rohilla@gmail.com>
2022-10-29 16:45:43 +0530

    Time: 7044 ms (45.65%), Space: 47.2 MB (67.39%) - LeetHub

---
+++ 2157-groups-of-strings/2157-groups-of-strings.py
@@ -0,0 +1,39 @@
+class DSU:
+    def __init__(self, n):
+        self.parent = [x for x in range(n)]
+        self.sizes = [1]*n
+    
+    def find(self, x):
+        if self.parent[x] == x: return x
+        self.parent[x] = self.find(self.parent[x])
+        return self.parent[x]
+    
+    def union(self, a, b):
+        x, y = self.find(a), self.find(b)
+        if x == y: return
+        self.parent[y] = x
+        self.sizes[x] += self.sizes[y]
+        self.sizes[y] = 0
+        
+class Solution:
+    def groupStrings(self, words: List[str]) -> List[int]:
+        seen = {}
+        dsu = DSU(len(words))
+        
+        for i, word in enumerate(words):
+            bitset = sum(1 << (ord(char)-ord("a")) for char in word)
+            if bitset in seen:
+                dsu.union(i, seen[bitset])
+            
+            for j in range(26):
+                if bitset & (1 << j):
+                    after_removing_set = bitset ^ (1 << j)
+                    if after_removing_set in seen:
+                        dsu.union(i, seen[after_removing_set])
+                    seen[after_removing_set] = i
+            seen[bitset] = i
+        
+        for i in range(len(words)):
+            dsu.find(i)
+        
+        return [len(set(dsu.parent)), max(dsu.sizes)]
