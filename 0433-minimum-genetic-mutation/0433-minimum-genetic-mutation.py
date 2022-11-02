d1c346c2ab186d58b40dd84832be6c08709f9174
Parth Rohilla <parth05rohilla@gmail.com>
2022-11-02 18:01:13 +0530

    Time: 55 ms (53.55%), Space: 13.9 MB (86.98%) - LeetHub

--- 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.py
+++ 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.py
@@ -2,32 +2,27 @@ class Solution:
     def minMutation(self, start: str, end: str, bank: List[str]) -> int:
         graph = defaultdict(list)
         bank = set(bank)
-        
         bank.add(start)
+        # Create a Graph from possible mutations - Change each character and add those to adjacency matrix that are in mutation_bank
         for gene in bank:
             for i in range(8):
                 temp = list(gene)
                 for new_char in "ACGT":
                     temp[i] = new_char
                     mutated_gene = "".join(temp)
-                    # print(mutated_gene)
                     if mutated_gene != gene and mutated_gene in bank:
                         graph[gene].append(mutated_gene)
-        
-        print(graph)
+        # Generic BFS traversal - Just count minimum edges to reach the "end" string
         Q = deque()
-        Q.append((start, 0))
-        visited = set()
-        visited.add(start)
+        Q.append((start,0))
+        visited = {start}
         while Q:
-            current, distance = Q.popleft()
-                
+            current, distance = Q.popleft() 
             for nei in graph[current]:
                 if nei == end:
                     return distance + 1
                 if nei not in visited:
                     Q.append((nei, distance + 1))
                     visited.add(nei)
-        
         return -1
         
