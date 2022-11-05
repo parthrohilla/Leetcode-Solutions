97a5a58aa26098b18f648b8f4b90ee90d3be9dff
Parth Rohilla <parth05rohilla@gmail.com>
2022-11-05 19:48:26 +0530

    Time: 1579 ms (16.75%), Space: 15.1 MB (15.35%) - LeetHub

---
+++ 0514-freedom-trail/0514-freedom-trail.py
@@ -0,0 +1,33 @@
+class Solution:
+    def findRotateSteps(self, ring: str, key: str) -> int:
+        n = len(ring)
+            
+        Q = deque()
+        Q.append([0,0,0]) #[node, rotations, found_word]
+        visited = set()
+        
+        while Q:
+            i, rotations, found = Q.popleft()
+            
+            if (i, found) in visited:
+                continue
+            
+            visited.add((i,found))
+            
+            while found < len(key) and key[found] == ring[i]:
+                found += 1
+            
+            if found == len(key):
+                return rotations + len(key)
+                
+            if i == 0:
+                Q.append([i+1, rotations + 1, found])
+                Q.append([n-1, rotations + 1, found])
+            elif i == n-1:
+                Q.append([i-1, rotations + 1, found])
+                Q.append([0, rotations + 1, found])
+            else:
+                Q.append([i+1, rotations + 1, found])
+                Q.append([i-1, rotations + 1, found])
+        
+        return 0
