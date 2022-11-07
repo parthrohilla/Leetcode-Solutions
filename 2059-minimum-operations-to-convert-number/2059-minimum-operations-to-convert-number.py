17cc68a8a79213abb76878bc8a3e003a729d5d38
Parth Rohilla <parth05rohilla@gmail.com>
2022-11-07 20:05:20 +0530

    Time: 2872 ms (69.46%), Space: 14.2 MB (70.99%) - LeetHub

---
+++ 2059-minimum-operations-to-convert-number/2059-minimum-operations-to-convert-number.py
@@ -0,0 +1,22 @@
+class Solution:
+    def minimumOperations(self, nums: List[int], start: int, goal: int) -> int:
+        seen = [False]*1001
+        Q = deque()
+        
+        Q.append([start,0])
+        seen[start] = True
+        
+        while Q:
+            curr, operations = Q.popleft()
+            
+            for n in nums:
+                new_states = [curr + n, curr - n, curr ^ n]
+                for S in new_states:
+                    if S == goal:
+                        return operations + 1
+                    
+                    if 0<=S<=1000 and not seen[S]:
+                        Q.append([S, operations + 1])
+                        seen[S] = True
+            
+        return -1
