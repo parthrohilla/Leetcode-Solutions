ce9698b5961eef8a74e4ebe1a7f9fe9eb631f7ea
Parth Rohilla <parth05rohilla@gmail.com>
2022-11-07 13:05:02 +0530

    Time: 650 ms (92.47%), Space: 15.2 MB (86.70%) - LeetHub

--- 0752-open-the-lock/0752-open-the-lock.py
+++ 0752-open-the-lock/0752-open-the-lock.py
@@ -2,20 +2,13 @@ class Solution:
     def openLock(self, deadends: List[str], target: str) -> int:
         deadends = set(deadends)
         Q = deque()
-        
         Q.append(["0000",0])
-        
-        visited = set()
-        visited.add("0000")
+        visited = {"0000"}
         
         while Q:
             curr, steps = Q.popleft()
-            
-            if curr == target:
-                return steps
-            
-            if curr in deadends:
-                continue
+            if curr == target: return steps
+            if curr in deadends: continue
             
             for i in range(4):
                 digit = int(curr[i])
