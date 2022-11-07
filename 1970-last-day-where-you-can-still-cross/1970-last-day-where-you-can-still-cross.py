088869b7d21f2311d0954feaa17bd748953638a6
Parth Rohilla <parth05rohilla@gmail.com>
2022-11-08 03:07:16 +0530

    Time: 5221 ms (48.73%), Space: 29.5 MB (10.15%) - LeetHub

--- 1970-last-day-where-you-can-still-cross/1970-last-day-where-you-can-still-cross.py
+++ 1970-last-day-where-you-can-still-cross/1970-last-day-where-you-can-still-cross.py
@@ -1,6 +1,6 @@
 class Solution:
     def latestDayToCross(self, row: int, col: int, cells: List[List[int]]) -> int:
-        # DSU Functions 
+        # DSU Implementation 
         def find(x):
             if parent[x] == x: return x
             parent[x] = find(parent[x])
@@ -10,23 +10,24 @@ class Solution:
             x, y = find(a), find(b)
             if x == y: return
             parent[y] = x
-            
+        
+        # Setting each cell as it's own parent before we start to Union anuy Cells
         parent, seen = {}, set()
         for i in range(row):
             for j in range(col):
                 parent[(i,j)] = (i,j)
         
+        # Declaring important Variables
         left, right = -1, -2
         parent[left], parent[right] = left, right
         directions = [[-1,0], [1,0], [0,-1], [0,1], [-1,1], [1,1], [-1,-1], [1,-1]]
         
+        # We Try to union any 2 connected water(flooded) cells as we traverse 
         for t, (x,y) in enumerate(cells):
             x -= 1
             y -= 1
-            
             for dx, dy in directions:
                 i, j = x + dx, y + dy
-                
                 if i in range(row):
                     if j in range(col):
                         if (i,j) in seen: union((i,j), (x,y))
