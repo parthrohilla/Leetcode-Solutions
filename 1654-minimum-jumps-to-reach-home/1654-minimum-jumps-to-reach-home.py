06c55a91dfa1d1aa1eaaea809ae17c60f2668d85
Parth Rohilla <parth05rohilla@gmail.com>
2022-11-07 17:57:47 +0530

    Time: 340 ms (29.47%), Space: 15.5 MB (24.78%) - LeetHub

--- 1654-minimum-jumps-to-reach-home/1654-minimum-jumps-to-reach-home.py
+++ 1654-minimum-jumps-to-reach-home/1654-minimum-jumps-to-reach-home.py
@@ -1,11 +1,11 @@
 class Solution:
     def minimumJumps(self, forbidden: List[int], a: int, b: int, x: int) -> int:
         forbidden, visited, Q = set(forbidden), set(), deque()
-        Q.append([0,0,True])
+        Q.append([0,0,True]) # [current_position, steps_taken_until_now, flag_for_saving_state_if_we_can_move_backward_from_this_position]
         threshold = max(forbidden) + x + a + b
         
         while Q:
-            curr, steps, flag = Q.popleft()
+            curr, steps, used = Q.popleft()
             
             if curr == x:
                 return steps
@@ -14,7 +14,7 @@ class Solution:
                 Q.append([curr+a, steps+1, False])
                 visited.add((curr+a, False))
             
-            if (curr - b, True) not in visited and (curr - b) not in forbidden and not flag and (curr - b) > 0 :
+            if (curr - b, True) not in visited and (curr - b) not in forbidden and not used and (curr - b) > 0 :
                 Q.append([curr-b,steps + 1,True])
                 visited.add((curr-b, True))
                          
