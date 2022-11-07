7f5fb9f2ba8424d01bd09b8998954866ad682a16
Parth Rohilla <parth05rohilla@gmail.com>
2022-11-07 21:31:20 +0530

    Time: 1413 ms (84.23%), Space: 35.8 MB (18.68%) - LeetHub

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.py
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.py
@@ -1,26 +1,26 @@
 class Solution:
     def minOperations(self, nums: List[int], x: int) -> int:
         P = {}
-        running = 0
+        prefix_left = 0
         P[0] = 0
         
         for i, num in enumerate(nums):
-            running += num
-            P[running] = i + 1
+            prefix_left += num
+            P[prefix_left] = i + 1
             
-        running = 0
+        prefix_right = 0
         ans = math.inf
         
         if x in P:
             ans = min(ans, P[x])
         
         for j, num in enumerate(nums[::-1]):
-            running += num
+            prefix_right += num
             
-            if running == x:
+            if prefix_right == x:
                 ans = min(ans, j+1)
             
-            if x - running in P and P[x-running] + j + 1 < len(nums):
-                ans = min(ans, P[x-running] + j + 1)
+            if x - prefix_right in P and P[x-prefix_right] + j + 1 < len(nums):
+                ans = min(ans, P[x-prefix_right] + j + 1)
             
         return ans if ans < math.inf else -1
