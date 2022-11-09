f8e4b7fb34470aa011e8739cfec1cf02cd7b7616
Parth Rohilla <parth05rohilla@gmail.com>
2022-11-09 18:11:27 +0530

    Time: 4569 ms (45.46%), Space: 16.5 MB (66.21%) - LeetHub

--- 2065-maximum-path-quality-of-a-graph/2065-maximum-path-quality-of-a-graph.py
+++ 2065-maximum-path-quality-of-a-graph/2065-maximum-path-quality-of-a-graph.py
@@ -6,16 +6,23 @@ class Solution:
             G[v].append([u,w])
             
         def dfs(current, path, left):
+            # We calculate the total path quality when we reach Node 0
+            # So the most trivial case would be when we just call dfs() the first time
+            # Later on, if we end up tracking back to Node 0 Again, we have a candidate for a "valid path"
+            # So we add up values for all the nodes in the set
+            # We continue our dfs() until we have some time left
             if current == 0:
                 tmp = 0
                 for node in path:
                     tmp += values[node]
                 self.best = max(self.best, tmp)
-            
+            # Moving to neighbour nodes and calling dfs() until time left
             for nei, wei in G[current]:
                 if wei <= left:
                     dfs(nei, {nei} | path, left - wei)
         
         self.best = 0
+        # Calling helper dfs() method
+        # arguments signify : current node, current path set, time left 
         dfs(0, {0}, maxTime)
         return self.best
