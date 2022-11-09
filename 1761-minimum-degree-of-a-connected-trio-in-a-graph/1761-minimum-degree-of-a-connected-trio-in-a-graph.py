a1435cd58abd3b09794795547e02495a36de8057
Parth Rohilla <parth05rohilla@gmail.com>
2022-11-09 15:26:25 +0530

    Time: 10800 ms (13.20%), Space: 35.4 MB (86.81%) - LeetHub

--- 1761-minimum-degree-of-a-connected-trio-in-a-graph/1761-minimum-degree-of-a-connected-trio-in-a-graph.py
+++ 1761-minimum-degree-of-a-connected-trio-in-a-graph/1761-minimum-degree-of-a-connected-trio-in-a-graph.py
@@ -1,15 +1,17 @@
 class Solution:
     def minTrioDegree(self, n: int, edges: List[List[int]]) -> int:
-        deg = [0]*n
-        G = [[False]*n for _ in range(n)]
+        deg = [0]*n # For Storing Degrees of nodes
+        G = [[False]*n for _ in range(n)] # Adjacency Matrix as we need to check connectivity between any 2 nodes in O(1)
         
+        # Filling degree list and connectivity Matrix for faster lookup
         for u,v in edges:
             u, v = u-1, v-1
             G[u][v] = G[v][u] = True
             deg[u] += 1
             deg[v] += 1
-            
-            
+    
+        # Try for all combinations of 3 Nodes and check if they form a trio 
+        # i.e. for any 3 nodes i, j, k in trio G[i][j] = G[j][k] = G[i][k] = True
         ans = math.inf
         for i in range(n):
             for j in range(i+1,n):
