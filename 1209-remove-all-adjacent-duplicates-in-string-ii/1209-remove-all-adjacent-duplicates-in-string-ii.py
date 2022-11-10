1b3ef15d80f01517cd95210e4b1e9c210e899a5e
Parth Rohilla <parth05rohilla@gmail.com>
2022-11-10 15:38:29 +0530

    Time: 228 ms (66.94%), Space: 18.7 MB (37.50%) - LeetHub

--- 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.py
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.py
@@ -3,7 +3,6 @@ class Solution:
         stack = []
         
         for char in s:
-            
             if stack and stack[-1][0] == char:
                 last, freq = stack.pop()
                 stack.append([char, freq+1])
@@ -11,12 +10,12 @@ class Solution:
                 stack.append([char,1])
             
             while stack and stack[-1][1] >= k:
-                temp, f = stack.pop()
-                if f - k > 0:
-                    stack.append([temp,f - k])
+                current, frequency = stack.pop()
+                if frequency - k > 0:
+                    stack.append([current,f - k])
                 if len(stack) >= 2 and stack[-1][0] == stack[-2][0]:
                     top, f1 = stack.pop()
-                    sec, f2 = stack.pop()
+                    _, f2 = stack.pop()
                     stack.append([top, f1 + f2])
                     
         ans = ""
