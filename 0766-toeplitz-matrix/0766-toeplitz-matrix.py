ea5a720bd7e114e27d0d002faf3aba010941ec28
Parth Rohilla <parth05rohilla@gmail.com>
2022-11-11 02:10:29 +0530

    Time: 247 ms (5.07%), Space: 13.9 MB (78.33%) - LeetHub

--- 0766-toeplitz-matrix/0766-toeplitz-matrix.py
+++ 0766-toeplitz-matrix/0766-toeplitz-matrix.py
@@ -5,7 +5,9 @@ class Solution:
         for i in range(m):
             for j in range(n):
                 for k in range(n):
-                    if i+k < m and j + k < n and M[i][j] != M[i+k][j+k]:
+                    if i + k >= m or j + k >= n:
+                        break
+                    elif M[i][j] != M[i+k][j+k]:
                         return False
         
         return True
