569465870a6637f8aa33bbadaaa1544f53c236f5
Parth Rohilla <parth05rohilla@gmail.com>
2022-11-10 18:22:16 +0530

    Time: 10606 ms (27.06%), Space: 14 MB (9.43%) - LeetHub

--- 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.py
+++ 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.py
@@ -1,16 +1,19 @@
 class Solution:
     def beautySum(self, s: str) -> int:
         beauty = 0
+        
         for i in range(len(s)):
             freq = [0]*26
             for j in range(i, len(s)):
-                lf = math.inf
                 freq[ord(s[j]) - ord("a")] += 1
-                mf = max(freq)
-                for f in freq:
-                    if 0 < f < lf:
-                        lf = f
-                        
-                beauty += (mf-lf)
+                most_frequenct = max(freq)
+                least_frequent = math.inf
+                
+                for x in freq:
+                    if 0 < x < least_frequent:
+                        least_frequent = x      
+                
+                beauty += (most_frequenct - least_frequent)
+        
         return beauty
                 
