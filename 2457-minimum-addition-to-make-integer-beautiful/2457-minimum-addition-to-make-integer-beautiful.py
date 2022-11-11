1e80968ddb911c0f76add134191c8fea2b1c8dd4
Parth Rohilla <parth05rohilla@gmail.com>
2022-11-12 03:49:26 +0530

    Time: 32 ms (94.23%), Space: 13.9 MB (76.78%) - LeetHub

--- 2457-minimum-addition-to-make-integer-beautiful/2457-minimum-addition-to-make-integer-beautiful.py
+++ 2457-minimum-addition-to-make-integer-beautiful/2457-minimum-addition-to-make-integer-beautiful.py
@@ -4,7 +4,7 @@ class Solution:
         base = 1
         
         def helper(num):
-            return sum(map(int,list(str(num))))
+            return sum(map(int,str(num)))
         
         while helper(N) > target:
             N = (N // 10 + 1)
