c57bc1fddcce2d587329eb6d58859fc9997a7abc
Parth Rohilla <parth05rohilla@gmail.com>
2022-11-11 19:49:28 +0530

    Time: 101 ms (53.92%), Space: 13.9 MB (60.78%) - LeetHub

--- 1005-maximize-sum-of-array-after-k-negations/1005-maximize-sum-of-array-after-k-negations.py
+++ 1005-maximize-sum-of-array-after-k-negations/1005-maximize-sum-of-array-after-k-negations.py
@@ -3,12 +3,10 @@ class Solution:
         nums.sort()
         print(nums)
         for i, n in enumerate(nums):
-            if n == 0 or k == 0:
-                return sum(nums)
-            elif n < 0:
+            if n == 0 or k == 0: return sum(nums)
+            elif n > 0: break
+            else:
                 nums[i] *= -1
                 k -= 1
-            else:
-                break
         
         return sum(nums) - ( k % 2) * 2 *min(nums)
