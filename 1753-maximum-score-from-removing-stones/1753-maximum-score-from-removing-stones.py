ee3fe8cbb9a3ab1f00c09bf2d0ff30e9fa3b986d
Parth Rohilla <parth05rohilla@gmail.com>
2022-11-16 14:07:58 +0530

    Time: 40 ms (94.89%), Space: 13.9 MB (81.08%) - LeetHub

--- 1753-maximum-score-from-removing-stones/1753-maximum-score-from-removing-stones.py
+++ 1753-maximum-score-from-removing-stones/1753-maximum-score-from-removing-stones.py
@@ -1,3 +1,6 @@
 class Solution:
     def maximumScore(self, a: int, b: int, c: int) -> int:
-        return min( (a+b+c)//2 , a + b + c - max(a,b,c))
+        S = sorted([a,b,c])
+        
+        if S[2] > S[0] + S[1]: return S[0] + S[1]
+        return sum(S)//2
