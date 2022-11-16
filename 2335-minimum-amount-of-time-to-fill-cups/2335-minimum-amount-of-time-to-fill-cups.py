1db11f7588c23a27621e68d26187a77021b465b7
Parth Rohilla <parth05rohilla@gmail.com>
2022-11-16 15:15:30 +0530

    Time: 52 ms (75.77%), Space: 13.8 MB (97.21%) - LeetHub

--- 2335-minimum-amount-of-time-to-fill-cups/2335-minimum-amount-of-time-to-fill-cups.py
+++ 2335-minimum-amount-of-time-to-fill-cups/2335-minimum-amount-of-time-to-fill-cups.py
@@ -3,7 +3,4 @@ class Solution:
         amount.sort()
         if amount[2] > amount[0] + amount[1]:
             return amount[2]
-        else:
-            s = sum(amount)
-            if s % 2 == 0: return s//2
-            else: return s//2 + 1
+        return (sum(amount) + 1) // 2
