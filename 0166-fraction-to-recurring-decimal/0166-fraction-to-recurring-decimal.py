7f162056e05dc91ab681d137d59f7a5bf99ba76a
Parth Rohilla <parth05rohilla@gmail.com>
2022-11-16 17:11:27 +0530

    Time: 60 ms (42.57%), Space: 14 MB (33.84%) - LeetHub

---
+++ 0166-fraction-to-recurring-decimal/0166-fraction-to-recurring-decimal.py
@@ -0,0 +1,35 @@
+class Solution:
+    def fractionToDecimal(self, N: int, D: int) -> str:
+        ans = ""
+        
+        if N == 0:
+            return "0"
+        
+        if N*D < 0:
+            ans += "-"
+        
+        N = abs(N)
+        D = abs(D)
+        
+        q = N // D
+        ans += str(q)
+        rem = N % D
+        
+        if rem == 0:
+            return ans
+        
+        ans += "."
+        seen = {}
+        
+        while rem != 0:
+            if rem in seen:
+                i = seen[rem]
+                return (ans[:i] + "(" + ans[i:] + ")")
+            
+            seen[rem] = len(ans)
+            rem *= 10
+            q = rem // D
+            ans += str(q)
+            rem = rem % D
+    
+        return ans
