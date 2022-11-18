5a787909044713967fd1c1020468257d066e7937
Parth Rohilla <parth05rohilla@gmail.com>
2022-11-18 19:19:32 +0530

    Time: 348 ms (72.58%), Space: 14.6 MB (55.07%) - LeetHub

--- 0654-maximum-binary-tree/0654-maximum-binary-tree.py
+++ 0654-maximum-binary-tree/0654-maximum-binary-tree.py
@@ -9,19 +9,11 @@ class Solution:
         
         def dfs(A):
             if not A: return None
-            i, num = largest(A)
+            i, num = max(enumerate(A), key = lambda x: x[1])
             L = dfs(A[:i])
             R = dfs(A[i+1:])
             root = TreeNode(num, L, R)
             return root
-        
-        def largest(A):
-            m = [0,-math.inf]
-            for i, num in enumerate(A):
-                if num > m[1]:
-                    m[0] = i
-                    m[1] = num
-            return m
         
         return dfs(nums)
         
