f457d9f2c21f408b757a0a90d5156b08ce23da1b
Parth Rohilla <parth05rohilla@gmail.com>
2022-11-18 21:46:39 +0530

    Time: 121 ms (83.75%), Space: 17.8 MB (62.28%) - LeetHub

--- 1315-sum-of-nodes-with-even-valued-grandparent/1315-sum-of-nodes-with-even-valued-grandparent.py
+++ 1315-sum-of-nodes-with-even-valued-grandparent/1315-sum-of-nodes-with-even-valued-grandparent.py
@@ -8,23 +8,12 @@ class Solution:
     def sumEvenGrandparent(self, root: TreeNode) -> int:
         self.ans = 0
         
-        def dfs(node):
+        def dfs(node, P, G):
             if not node: return
-            if node.val % 2 == 0: addGrandChildren(node)
-            dfs(node.left)
-            dfs(node.right)
-        
-        def addGrandChildren(node):
-            if not node: return
-            if not node.left and not node.right: return
-            
-            if node.left:
-                if node.left.left: self.ans += node.left.left.val
-                if node.left.right: self.ans += node.left.right.val
-                    
-            if node.right:
-                if node.right.left: self.ans += node.right.left.val
-                if node.right.right: self.ans += node.right.right.val
-                    
-        dfs(root)
+            if G and G.val % 2 == 0:
+                self.ans += node.val
+            dfs(node.left, node, P)
+            dfs(node.right, node, P)
+                        
+        dfs(root, None, None)
         return self.ans
