bac501c65bf5f57ef9b66923193a3da664d27741
Parth Rohilla <parth05rohilla@gmail.com>
2022-11-21 15:24:47 +0530

    Time: 2835 ms (57.14%), Space: 157.8 MB (14.29%) - LeetHub

---
+++ 2476-closest-nodes-queries-in-a-binary-search-tree/2476-closest-nodes-queries-in-a-binary-search-tree.py
@@ -0,0 +1,29 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def closestNodes(self, root: Optional[TreeNode], queries: List[int]) -> List[List[int]]:
+        def inorder(node):
+            if not node: return
+            inorder(node.left)
+            A.append(node.val)
+            inorder(node.right)
+        
+        A = []
+        inorder(root)
+        N = len(A)
+        ans = []
+        
+        for q in queries:
+            i = bisect_left(A,q)
+            if i < N and A[i] == q: ans.append([q,q])
+            else:
+                if i == 0: ans.append([-1, A[i]])
+                elif i == N: ans.append([A[-1], -1])
+                else: ans.append([A[i-1], A[i]])
+        
+        return ans
+        
