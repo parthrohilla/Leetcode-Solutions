31c35bff9eb01cd6a7043cdab85a5b64b629b5ad
Parth Rohilla <parth05rohilla@gmail.com>
2022-11-24 17:41:18 +0530

    Time: 162 ms (30.89%), Space: 14 MB (46.36%) - LeetHub

--- 0445-add-two-numbers-ii/0445-add-two-numbers-ii.py
+++ 0445-add-two-numbers-ii/0445-add-two-numbers-ii.py
@@ -8,42 +8,27 @@ class Solution:
         
         def reverse(head):
             if not head or not head.next: return head
-            small = reverse(head.next)
+            T = reverse(head.next)
             head.next.next = head
             head.next = None
-            return small
+            return T
         
         A = reverse(l1)
         B = reverse(l2)
         C = ListNode(val = -1)
         
-        i, j, k = A, B, C
-        carry = 0
-        while i and j:
-            S = i.val + j.val + carry
+        i, j, k, carry = A, B, C, 0
+        while i or j:
+            X = i.val if i else 0
+            Y = j.val if j else 0
+            S = X + Y + carry
             carry = S // 10
             k.next = ListNode(S % 10)
-            i = i.next
-            j = j.next
+            i = i.next if i else None
+            j = j.next if j else None
             k = k.next
         
-        while i:
-            S = i.val + carry
-            carry = S // 10
-            k.next = ListNode(S % 10)
-            i = i.next
-            k = k.next
-        
-        while j:
-            S = j.val + carry
-            carry = S // 10
-            k.next = ListNode(S % 10)
-            j = j.next
-            k = k.next
-        
-        if carry:
-            k.next = ListNode(carry)
-            
+        if carry: k.next = ListNode(carry)   
         return reverse(C.next)
             
             
