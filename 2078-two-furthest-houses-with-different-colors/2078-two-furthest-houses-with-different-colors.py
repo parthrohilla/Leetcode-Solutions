33894cc918f3343b7807630fade00be46afa3c3a
Parth Rohilla <parth05rohilla@gmail.com>
2022-11-24 18:30:18 +0530

    Time: 77 ms (51.53%), Space: 18.1 MB (15.98%) - LeetHub

--- 2078-two-furthest-houses-with-different-colors/2078-two-furthest-houses-with-different-colors.py
+++ 2078-two-furthest-houses-with-different-colors/2078-two-furthest-houses-with-different-colors.py
@@ -3,11 +3,8 @@ class Solution:
         @lru_cache(None)
         def dfs(A):
             ans = 0
-            if len(A) > 1 and A[0] != A[-1]:
-                ans = max(ans, len(A)-1)
-            else:
-                ans = max(ans, dfs(A[:-1]), dfs(A[1:]))
+            if len(A) > 1 and A[0] != A[-1]: ans = max(ans, len(A)-1)
+            else: ans = max(ans, dfs(A[:-1]), dfs(A[1:]))
             return ans
-        
         return dfs(tuple(colors))
     
