1d6114f001a104609a3741df76bbbf61120fb0f6
Parth Rohilla <parth05rohilla@gmail.com>
2022-11-25 20:34:35 +0530

    Time: 1101 ms (41.87%), Space: 16.4 MB (59.40%) - LeetHub

--- 0524-longest-word-in-dictionary-through-deleting/0524-longest-word-in-dictionary-through-deleting.py
+++ 0524-longest-word-in-dictionary-through-deleting/0524-longest-word-in-dictionary-through-deleting.py
@@ -2,7 +2,7 @@ class Solution:
     def findLongestWord(self, s: str, dictionary: List[str]) -> str:
         dictionary.sort(key = lambda x: (-len(x),x))
         
-        def possible(word):
+        def isSunsequence(word):
             i, j = 0, 0
             while i < len(s):
                 if s[i] == word[j]: j += 1
@@ -11,6 +11,7 @@ class Solution:
             return False
         
         for W in dictionary:
-            if possible(W):
+            if isSunsequence(W):
                 return W
+        
         return ""
