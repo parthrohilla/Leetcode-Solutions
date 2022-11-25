0ebbac2d5ec8b88b7c795303841e23dd5a27efe2
Parth Rohilla <parth05rohilla@gmail.com>
2022-11-25 20:10:05 +0530

    Time: 1709 ms (8.48%), Space: 19 MB (34.26%) - LeetHub

--- 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.py
+++ 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.py
@@ -1,16 +1,33 @@
 class Solution:
-    def sumSubarrayMins(self, A: List[int]) -> int:
-        A = [0] + A
-        result = [0]*len(A)
-        stack = [0]
+    def sumSubarrayMins(self, arr: List[int]) -> int:
+        mod = 1000000007
+
+        # Right boundary of i, where arr[i] is the minima.
+        leftToRight = [len(arr)] * len(arr)
         
-        for i in range(len(A)):
-            while A[stack[-1]] > A[i]:
-                stack.pop()
-            j = stack[-1]
-            result[i] = result[j] + (i-j)*A[i]
+        # Going from left to right to find the right boundary.
+        stack = []
+        for i in range(len(arr)):
+            while stack and arr[i] <= arr[stack[-1]]:
+                leftToRight[stack.pop()] = i
             stack.append(i)
         
-        return sum(result) % (10**9 + 7)
+        # Left boundary of i, where arr[i] is the minima.
+        rightToLeft = [-1] * len(arr)
+        # Going from right to left to find the left boundary.
+        stack = []
+        for i in reversed(range(len(arr))):
+            while stack and arr[i] < arr[stack[-1]]:
+                rightToLeft[stack.pop()] = i
+            stack.append(i)
+
+        res = 0
+        # Compute the result.
+        for idx in range(len(arr)):
+            l, r = rightToLeft[idx], leftToRight[idx]
+            res += (r-idx) * (idx-l) * arr[idx]
+            res %= mod
+            
+        return res
                 
         
