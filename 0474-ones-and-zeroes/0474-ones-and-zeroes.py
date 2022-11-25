deb1e82105c151cad5aaa19ff60ee0778841adba
Parth Rohilla <parth05rohilla@gmail.com>
2022-11-25 21:09:24 +0530

    Time: 4209 ms (74.81%), Space: 198.6 MB (22.60%) - LeetHub

--- 0474-ones-and-zeroes/0474-ones-and-zeroes.py
+++ 0474-ones-and-zeroes/0474-ones-and-zeroes.py
@@ -1,14 +1,18 @@
 class Solution:
     def findMaxForm(self, strs: List[str], m: int, n: int) -> int:
-        xy = [[s.count("0"), s.count("1")] for s in strs]
+        zeros_ones = [[s.count("0"), s.count("1")] for s in strs]
         
         @lru_cache(None)
         def dfs(index, ones, zeros):
             if index >= len(strs): return 0
+            
             pick = -1
+            Z, O = zeros_ones[index]
+            
             ignore = dfs(index + 1, ones, zeros)
-            z,o = xy[index]
-            if ones + o <= n and zeros + z <= m:
-                pick = 1 + dfs(index + 1, ones + o, zeros + z)
+            if ones + O <= n and zeros + Z <= m:
+                pick = 1 + dfs(index + 1, ones + O, zeros + Z)
+                
             return max(pick, ignore)
+        
         return dfs(0,0,0)
